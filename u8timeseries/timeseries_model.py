bfd5c8a51a1c51981825015e4926d9546aeb1890
Julien Herzen <julien.herzen@unit8.co>
2018-09-27 14:05:16 +0200

    backtesting

--- u8timeseries/timeseries_model.py
+++ u8timeseries/timeseries_model.py
@@ -53,7 +53,7 @@ class TimeseriesModel(ABC):
         assert self.fit_called, 'predict() method called before fit()'
 
     def backtest(self, df, target_column, time_column, stepduration_str,
-                 start_dt, n, eval_fun, nr_steps_iter=1, point_prediction=False):
+                 start_dt, n, eval_fun, nr_steps_iter=1, predict_nth_only=False):
         """
         Performs backtesting and returns the outputs of a user-provided evaluation function.
 
@@ -72,9 +72,10 @@ class TimeseriesModel(ABC):
         :param start_dt: the datetime corresponding to the beginning of the first validation set
         :param n: number of points in each validation sets
         :param eval_fun: a function of following form: eval_fun(true_values, predicted_values),
-                         which returns some evaluation of the prediction (typically, an error value)
+                         which returns some evaluation of the prediction (typically, an error value).
+                         The arguments (true_values, predicted_values) are lists.
         :param nr_steps_iter: the number of time steps to elapse between each validation set
-        :param point_prediction: If true, evaluates the predictions only on the n-th point of each validation set
+        :param predict_nth_only: If true, evaluates the predictions only on the n-th point of each validation set
 
         :return: A list containing the outputs of [eval_fun()] on all validation sets.
         """
@@ -82,25 +83,58 @@ class TimeseriesModel(ABC):
         # TODO: at some point we can get rid of this requirement
         assert time_column is not None, 'argument "time_column" must be provided for backtesting'
 
+        df_to_use = df[[time_column, target_column]]
+
+        results = []
+        current_pointer = start_dt
+        val_df = df_to_use[df_to_use[time_column] > current_pointer].iloc[:n, :]
+
+        while len(val_df) == n:
+            train_df = df_to_use[df_to_use[time_column] <= current_pointer]
+            self.fit(train_df, target_column, time_column, stepduration_str)
+            preds = list(self.predict(n=n)['yhat'])
+            self.__init__()  # re-init model, as fit() stores some state
+            y_true = list(val_df[target_column])
+
+            if predict_nth_only:
+                # We only care about the last point
+                preds = list([preds[-1]])
+                y_true = list([y_true[-1]])
+
+            results.append(eval_fun(y_true, preds))
+
+            # update pointer and validation set
+            current_pointer = self._add_time_delta_to_datetime(current_pointer, nr_steps_iter, stepduration_str)
+            val_df = df_to_use[df_to_use[time_column] > current_pointer].iloc[:n, :]
+
+        # if len(results) == 0:
+        #     logging.warning('Empty backtesting results: did you specify datetimes allowing at least 1 validation set?')
+        return results
+
+    def _add_time_delta_to_datetime(self, np_dt, i, stepduration_str):
+        """
+        Adds [i] time-steps (whose length is given by [stepduration_str]) to the numpy datetime [np_dt]
+        """
+        datet = pd.Timestamp(np_dt)
+
+        switch = {
+            'second': lambda dt: dt + relativedelta(seconds=+i),
+            'minute': lambda dt: dt + relativedelta(minutes=+i),
+            'hour': lambda dt: dt + relativedelta(hours=+i),
+            'day': lambda dt: dt + relativedelta(days=+i),
+            'week': lambda dt: dt + relativedelta(weeks=+i),
+            'month': lambda dt: dt + relativedelta(months=+i),
+            'year': lambda dt: dt + relativedelta(years=+i)
+        }
+        return switch[stepduration_str](datet)
+
     def _get_new_dates(self, n):
         """
         This function creates a list of the n new dates (after the end of training set)
         :param n: number of dates after training set to generate
         """
-        def _add_time_delta_to_datetime(np_dt, i):
-            datet = pd.Timestamp(np_dt)
-
-            switch = {
-                'second': lambda dt: dt + relativedelta(seconds=+i),
-                'minute': lambda dt: dt + relativedelta(minutes=+i),
-                'hour': lambda dt: dt + relativedelta(hours=+i),
-                'day': lambda dt: dt + relativedelta(days=+i),
-                'week': lambda dt: dt + relativedelta(weeks=+i),
-                'month': lambda dt: dt + relativedelta(months=+i),
-                'year': lambda dt: dt + relativedelta(years=+i)
-            }
-            return switch[self.stepduration_str](datet)
-        return [_add_time_delta_to_datetime(self.training_dates[-1], i) for i in range(1, n + 1)]
+        return [self._add_time_delta_to_datetime(self.training_dates[-1], i, self.stepduration_str)
+                for i in range(1, n + 1)]
 
     def _build_forecast_df(self, point_preds, lower_bound=None, upper_bound=None):
         """
