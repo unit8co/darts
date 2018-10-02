c770188e5b4f33f21bcd17653241adc8696f855a
Julien Herzen <julien.herzen@unit8.co>
2018-10-02 11:53:00 +0200

    more backtesting, better supervised model

--- u8timeseries/timeseries_model.py
+++ u8timeseries/timeseries_model.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
-from dateutils import relativedelta
 import pandas as pd
 import logging
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from .utils import add_time_delta_to_datetime, get_new_dates, fill_dates_between
+from .backtesting import backtest
+
+
+ALLOWED_STEP_DURATIONS = {'second', 'minute', 'hour', 'day', 'week', 'month', 'quarter', 'year'}
+
+
+def assert_step_duration(stepduration_str):
+    assert stepduration_str in ALLOWED_STEP_DURATIONS, \
+        'step duration must be in {}'.format(ALLOWED_STEP_DURATIONS)
 
 
 class TimeseriesModel(ABC):
@@ -11,8 +22,6 @@ class TimeseriesModel(ABC):
 
     @abstractmethod
     def __init__(self):
-        self.allowed_stepduration_str = {'second', 'minute', 'hour', 'day', 'week', 'month', 'year'}
-
         # Stores training date information (if provided):
         self.training_dates = None
         self.time_column = None
@@ -33,8 +42,7 @@ class TimeseriesModel(ABC):
         TODO: Interpolate missing values
         """
         if time_column is not None:
-            assert stepduration_str in self.allowed_stepduration_str, \
-                   'periodicity_str argument must be in {}'.format(self.allowed_stepduration_str)
+            assert_step_duration(stepduration_str)
 
             self.time_column = time_column
             self.training_dates = df[time_column].values
@@ -55,104 +63,16 @@ class TimeseriesModel(ABC):
 
     def backtest(self, df, target_column, time_column, stepduration_str,
                  start_dt, n, eval_fun, nr_steps_iter=1, predict_nth_only=False):
-        """
-        Performs backtesting and returns the outputs of a user-provided evaluation function.
 
-        This function builds several validation sets, by iterating a pointer over the dataframe,
-        starting at [start_dt] and every [nr_steps_iter] time slot. The validation sets are the
-        [n] data points after the pointer. The function trains the model on the train set and emits
-        predictions for the [n] points of the validation set. It then calls [eval_fun()] on the
-        predicted values (and the targets), and returns a list containing the outputs of [eval_fun()]
-        on all validation sets.
+        # Prepare generic fit() and predict() calls to be used from backtest()
+        def fit_fn(*args):
+            return self.fit(*args)
 
-        :param df: a Pandas DataFrame that contains at least one column with time series values
-        :param target_column: the column containing time series values
-        :param time_column: *mandatory for backtesting*, the column containing corresponding timestamps
-        :param stepduration_str: this must provide the duration of time steps,
-               among following values: {'second', 'minute', 'hour', 'day', 'week', 'month', 'year'}
-        :param start_dt: the datetime corresponding to the beginning of the first validation set
-        :param n: number of points in each validation sets
-        :param eval_fun: a function of following form: eval_fun(true_values, predicted_values),
-                         which returns some evaluation of the prediction (typically, an error value).
-                         The arguments (true_values, predicted_values) are lists.
-        :param nr_steps_iter: the number of time steps to elapse between each validation set
-        :param predict_nth_only: If true, evaluates the predictions only on the n-th point of each validation set
+        def predict_fn(_, _n):
+            return self.predict(_n)
 
-        :return: A list containing the outputs of [eval_fun()] on all validation sets.
-        """
-
-        # TODO: at some point we can get rid of this requirement
-        assert time_column is not None, 'argument "time_column" must be provided for backtesting'
-
-        df_to_use = df[[time_column, target_column]]
-
-        def _update_pointer_and_val_set(old_pointer, nr_steps=0):
-            new_pointer = self._add_time_delta_to_datetime(old_pointer, nr_steps, stepduration_str)
-            new_val_df = df_to_use[df_to_use[time_column] > new_pointer].iloc[:n, :]
-            return new_pointer, new_val_df
-
-        current_pointer, current_val_df = _update_pointer_and_val_set(start_dt)
-        results = []
-
-        while len(current_val_df) == n:
-            train_df = df_to_use[df_to_use[time_column] <= current_pointer]
-
-            if len(train_df) == 0:
-                logging.warning('Skipping validation set starting on {}, as it yields empty training set.'
-                                .format(current_pointer))
-                current_pointer, current_val_df = _update_pointer_and_val_set(current_pointer, nr_steps_iter)
-                continue
-
-            try:
-                self.fit(train_df, target_column, time_column, stepduration_str)
-                preds = list(self.predict(n=n)['yhat'])
-                self.__init__()  # re-init model, as fit() stores some state
-
-                y_true = list(current_val_df[target_column])
-
-                if predict_nth_only:
-                    # We only care about the last point
-                    preds = list([preds[-1]])
-                    y_true = list([y_true[-1]])
-
-                results.append(eval_fun(y_true, preds))
-            except:
-                # TODO: proper handling
-                logging.warning('Something went wrong when training, for validation set starting on {}'
-                                .format(current_pointer))
-            finally:
-                # update pointer and validation set
-                current_pointer, current_val_df = _update_pointer_and_val_set(current_pointer, nr_steps_iter)
-
-        if len(results) == 0:
-            logging.warning('Empty backtesting results: did you specify datetimes allowing at least 1 validation set?')
-
-        return results
-
-    def _add_time_delta_to_datetime(self, np_dt, i, stepduration_str):
-        """
-        Adds [i] time-steps (whose length is given by [stepduration_str]) to the numpy datetime [np_dt]
-        """
-        datet = pd.Timestamp(np_dt)
-
-        switch = {
-            'second': lambda dt: dt + relativedelta(seconds=+i),
-            'minute': lambda dt: dt + relativedelta(minutes=+i),
-            'hour': lambda dt: dt + relativedelta(hours=+i),
-            'day': lambda dt: dt + relativedelta(days=+i),
-            'week': lambda dt: dt + relativedelta(weeks=+i),
-            'month': lambda dt: dt + relativedelta(months=+i),
-            'year': lambda dt: dt + relativedelta(years=+i)
-        }
-        return switch[stepduration_str](datet)
-
-    def _get_new_dates(self, n):
-        """
-        This function creates a list of the n new dates (after the end of training set)
-        :param n: number of dates after training set to generate
-        """
-        return [self._add_time_delta_to_datetime(self.training_dates[-1], i, self.stepduration_str)
-                for i in range(1, n + 1)]
+        return backtest(df, target_column, time_column, stepduration_str, start_dt, n, eval_fun, fit_fn,
+                        predict_fn, nr_steps_iter, predict_nth_only)
 
     def _build_forecast_df(self, point_preds, lower_bound=None, upper_bound=None):
         """
@@ -171,7 +91,7 @@ class TimeseriesModel(ABC):
 
         if self.time_column is not None:
             n = len(point_preds)
-            new_dates = self._get_new_dates(n)
+            new_dates = get_new_dates(self.training_dates[-1], n, self.stepduration_str)
             columns[self.time_column] = pd.Series(new_dates)
 
         if lower_bound is not None:
@@ -185,25 +105,119 @@ class TimeseriesModel(ABC):
         return pd.DataFrame(columns)
 
 
-class SupervisedTimeSeriesModel(ABC):
+# TODO: move?
+class SupervisedTimeSeriesModel:
 
-    @abstractmethod
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def fit(self, df, target_column, feature_columns=None):
+    def __init__(self, model=RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=0)):
         """
-        :param df: A Pandas DataFrame containing at least one feature column and one column with time series values (targets)
-        :param feature_columns: A list of columns to use as features. If None, use all columns.
+        :param model: The regression model to use. It can be any sklearn model with fit() and predict()
         """
-        pass
+        self.model = model
+        self.target_column = None
+        self.time_column = None
+        self.training_dates = None
+        self.fit_called = False
+        self.stepduration_str = None
 
-    @abstractmethod
+    def __str__(self):
+        return 'supervised ({})'.format(self.model)
+
+    def fit(self, df, target_column, time_column, stepduration_str=None, feature_columns=None):
+        """
+        Supports missing time steps
+
+        :param df: A Pandas DataFrame containing the training set. It has to contain at least one time column and one
+                   column with time series values (targets). Optionally, it can also contain extra features columns.
+        :param target_column: The name of the column containing the target values
+        :param time_column: The name of the column containing the timestamps. The timestamps can be of any type
+                            that is hashable and comparable. They will be encoded (using ordinal encoding) before
+                            being treated as a feature column by the regressor.
+                            If not provided, the time is assumed to be part of feature columns, and encoded
+                            by the user.
+        :param stepduration_str: the duration of a time step
+        :param feature_columns: A list of numerical columns to use as features. Should not contain the time column,
+                                unless if [time_column] is not provided and the time has already been encoded in a
+                                numerical type. If None, use all remaining columns.
+        """
+
+        assert len(df) >= 2, 'Need at least 2 data points'
+        assert time_column not in feature_columns, 'feature_columns must not contain the time column'
+
+        # clear any previous state
+        self.__init__(model=self.model)
+
+        # TODO: retain only numerical columns
+        X_train = df.drop([target_column], axis=1) if feature_columns is None else df[feature_columns]
+        self.target_column = target_column
+
+        if time_column is not None:
+            assert_step_duration(stepduration_str)
+
+            # Encode time column using ordinal encoding; possibly interpolating if some time steps are missing
+            le = LabelEncoder()
+            first_dt = min(df[time_column])
+            last_dt = max(df[time_column])
+            all_dts = fill_dates_between(first_dt, last_dt, stepduration_str)
+            le.fit(all_dts)
+            time_codes = le.transform(df[time_column])
+            X_train[time_column + '-u8ts_codes'] = time_codes
+            self.time_column = time_column
+            self.training_dates = X_train[time_column].values
+            self.stepduration_str = stepduration_str
+
+        y_train = df[target_column].values
+        self.fit_called = True
+        self.model.fit(X_train, y_train)
+
     def predict(self, df, feature_columns=None):
         """
         :param df: A Pandas DataFrame containing features columns
         :param feature_columns: A list of columns to use as features. If None, use all columns.
         :return: A Pandas Series containing the predictions for all rows in df
+
+        Note: it is the caller responsibility to ensure all columns used as feature contain only
+        data available at the corresponding values of the time column.
         """
-        pass
+
+        assert self.fit_called, 'predict() method called before fit()'
+
+        if self.target_column in df:
+            logging.warning('The test dataframe contains the target column; disregarding it.')
+        X_test = df.drop([self.target_column], axis=1) if feature_columns is None else df[feature_columns]
+
+        if self.time_column is not None:
+            # Apply an ordinal encoding consistent with training set
+            start_test_date = min(df[self.time_column])
+            start_train_date = min(self.training_dates)
+            end_test_date = max(df[self.time_column])
+            end_train_date = max(self.training_dates)
+
+            # If the test set comes before the training set, we raise an exception
+            if start_test_date < end_train_date:
+                raise ValueError('Some test dates provided to predict() are earlier than dates of training set.')
+
+            # Interpolate dates in between and use ordinal encoding consistent with training set
+            all_dates = fill_dates_between(start_train_date, end_test_date, self.stepduration_str)
+            le = LabelEncoder()
+            le.fit(all_dates)
+            test_time_codes = le.transform(X_test[self.time_column])
+            X_test[self.time_column + '-u8ts_codes'] = test_time_codes
+
+        predictions = self.model.predict(X_test)
+        to_return = df.copy()
+        to_return['yhat'] = predictions
+        # TODO: perhaps also confidence intervals, depending what is supported by underlying sklearn model
+        return to_return
+
+    def backtest(self, df, target_column, time_column, stepduration_str,
+                 start_dt, n, eval_fun, nr_steps_iter=1, predict_nth_only=False, feature_columns=None):
+
+        # Prepare generic fit() and predict() calls to be used from backtest()
+        def fit_fn(*args):
+            return self.fit(*args, feature_columns)
+
+        def predict_fn(val_set, _):
+            return self.predict(val_set, feature_columns)
+
+        return backtest(df, target_column, time_column, stepduration_str, start_dt, n, eval_fun, fit_fn,
+                        predict_fn, nr_steps_iter, predict_nth_only)
