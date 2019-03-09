392580e86411aa07d46fb55bd23cadd2d4ac4036
Julien Herzen <julien.herzen@unit8.co>
2019-03-09 18:41:27 +0100

    modify init

--- u8timeseries/__init__.py
+++ u8timeseries/__init__.py
@@ -2,5 +2,5 @@ from u8timeseries.models.arima import Arima, AutoArima
 from u8timeseries.models.baselines import KthValueAgoBaseline
 from u8timeseries.models.exponential_smoothing import ExponentialSmoothing
 from u8timeseries.models.prophet import Prophet
-from u8timeseries.models.timeseries_model import SupervisedTimeSeriesModel
 from .metrics import mape, mase, overall_percentage_error
+from .timeseries import TimeSeries
