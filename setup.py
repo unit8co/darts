697cc2eee5dea9edd8f66dd6bd2f05a17efd4b43
Krzysiek Styrc <kstyrc@unit8.co>
2019-05-28 12:06:53 +0200

    Save Croston progress

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='u8timeseries',
       version='0.1',
@@ -7,16 +7,17 @@ setup(name='u8timeseries',
       author='Unit8',
       author_email='info@unit8.co',
       license='Apache License 2.0',
-      packages=['u8timeseries'],
+      packages=find_packages(),
       install_requires=[
-          'dateutils',
-          'statsmodels',
-          'pyramid-arima',
-          'fbprophet',
-          'scikit-learn',
-          'pandas',
-          'tqdm',
-          'numpy==1.15.4'  # more recent not yet supported by Prophet
+          'dateutils>=0.6.6',
+          'statsmodels>=0.9.0',
+          'scipy==1.2.1',  # pdmarima requires 1.2.1, not 1.3.*
+          'pmdarima>=1.2.0',
+          'fbprophet>=0.5',
+          'scikit-learn>=0.21.2',
+          'pandas==0.20.1',  # new versions have plotting bug
+          'tqdm>=4.32.1',
+          'numpy>=1.15.4'
       ],
       zip_safe=False)
 
