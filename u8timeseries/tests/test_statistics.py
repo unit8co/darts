c367143e887a8c0a54599996d5311007a70c294f
MounBen <mounir.benheddi@gmail.com>
2019-12-06 17:03:18 +0100

    test(test_statistics): added another test

--- u8timeseries/tests/test_statistics.py
+++ u8timeseries/tests/test_statistics.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 from u8timeseries import TimeSeries
-from u8timeseries.models import statistics
+from u8timeseries.models.statistics import check_seasonality
 
 
 class TimeSeriesTestCase(unittest.TestCase):
@@ -11,10 +11,11 @@ class TimeSeriesTestCase(unittest.TestCase):
 
     def test_check_seasonality(self):
         pd_series = pd.Series(range(50), index=pd.date_range('20130101', '20130219'))
-        pd_series = pd_series.map(lambda x: np.sin(x*np.pi / 3))
+        pd_series = pd_series.map(lambda x: np.sin(x*np.pi / 3 + np.pi/2))
         series = TimeSeries(pd_series)
 
-        self.assertEqual((True, 6), statistics.check_seasonality(series))
+        self.assertEqual((True, 6), check_seasonality(series))
+        self.assertEqual((False, 3), check_seasonality(series, m=3))
 
 
 if __name__ == '__main__':
