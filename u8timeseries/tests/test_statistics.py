5ce5aff88ffb475505a5b9e88b24a1f0f5682572
MounBen <41362408+MounBen@users.noreply.github.com>
2019-12-10 10:33:31 +0100

    Feature/statistics (#25)
    * feat(statistics): added statistics.py
    
    Contains seasonality checks, adjustment and plot auto-correlation function
    
    * update(Air-passengers-example): added statistics
    
    Statiscal exploration of Air Passengers Data with plot_acf
    and check_seasonality
    
    * update(License): finalize terms
    
    * Revert "update(License): finalize terms"
    
    This reverts commit b8f47f1a1ce7db2f4b52e9956f5c0de8354af8df.
    Commit was not on master
    
    * fix(statistics): various fixes and upgrade
    
    Changed plot_acf to include significance values for each lag.
    Modified check_seasonality to detect the first significant spike.
    seasonal_adjustment has been split into pieces:
      - helper function remove_from_series
      - remove extract_trend_and_seasonality
      - remove_seasonality
      - remove_trend has been added
    
    * refactor(Air-passenger): new version with modified statisics
    
    Notebook as been re-ran so that it shows the improved functions
    in statistics.py
    
    * Update u8timeseries/models/statistics.py
    
    Optimized band computations in plot_acf
    
    Co-Authored-By: Charles Dupuy <34459131+Bugdup@users.noreply.github.com>
    
    * Update u8timeseries/models/statistics.py
    
    Set extrapolate_trend to 'freq' to avoid NaNs appearing in trend
    
    Co-Authored-By: Charles Dupuy <34459131+Bugdup@users.noreply.github.com>
    
    * fix(statistics): fixed check_seasonality
    
    Corrected the selection of candidates that induced some bugs.
    Changed the main body of check_seasonality with multiple seasonality in mind.
    
    * test(test_statistics): added another test
    
    * Update statistics.py
    
    Made the ACF error message clearer

---
+++ u8timeseries/tests/test_statistics.py
@@ -0,0 +1,22 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from u8timeseries import TimeSeries
+from u8timeseries.models.statistics import check_seasonality
+
+
+class TimeSeriesTestCase(unittest.TestCase):
+    __test__ = True
+
+    def test_check_seasonality(self):
+        pd_series = pd.Series(range(50), index=pd.date_range('20130101', '20130219'))
+        pd_series = pd_series.map(lambda x: np.sin(x*np.pi / 3 + np.pi/2))
+        series = TimeSeries(pd_series)
+
+        self.assertEqual((True, 6), check_seasonality(series))
+        self.assertEqual((False, 3), check_seasonality(series, m=3))
+
+
+if __name__ == '__main__':
+    unittest.main()
