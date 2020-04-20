49a0bee8da7a5b16e93dbb181c61d33441e744b0
Unit8 <unit8@MacBook-Air.local>
2020-04-20 09:34:42 +0200

    fix(logging): removed unnecessary line in get_logger

--- u8timeseries/custom_logging.py
+++ u8timeseries/custom_logging.py
@@ -8,7 +8,6 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     stderr_handler = logging.StreamHandler()
-    stderr_handler.setLevel(logging.INFO)
     formatter = logging.Formatter('[%(asctime)s] %(levelname)s | %(name)s | %(message)s')
     stderr_handler.setFormatter(formatter)
     logger.addHandler(stderr_handler)
