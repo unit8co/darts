68383bc2b2963f5327226f34211e8d04fc2d5eba
MounBen <mounir.benheddi@gmail.com>
2020-04-21 08:19:13 +0200

    fix(auto-doc):corrects conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '../../')
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
