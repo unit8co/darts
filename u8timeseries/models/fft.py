da04631e64f89c05b56ebba03411fa568323732a
Unit8 <unit8@MacBook-Air.local>
2020-04-27 17:21:57 +0200

    feature(fft): improved efficiency, changed arguments, added detrending

--- u8timeseries/models/fft.py
+++ u8timeseries/models/fft.py
@@ -2,6 +2,8 @@ from .autoregressive_model import AutoRegressiveModel
 from ..timeseries import TimeSeries
 from ..custom_logging import raise_if_not, time_log, get_logger
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 logger = get_logger(__name__)
 
@@ -27,8 +29,6 @@ def compare_seasonality(ts_1, ts_2, required_matches):
 
 
 def crop_to_match_seasons(series: 'TimeSeries', required_matches={'day', 'month'}):
-    
-    
     first_ts = series._series.index[0]
     freq = first_ts.freq
     pred_ts = series._series.index[-1] + freq
@@ -40,46 +40,60 @@ def crop_to_match_seasons(series: 'TimeSeries', required_matches={'day', 'month'
             new_series = series.drop_before(curr_ts)
             return new_series
     
-    logger.warning("No matchins time stamp could be found, returning original TimeSeries.")
+    logger.warning("No matching time stamp could be found, returning original TimeSeries.")
     return series
-
 
 class FFT(AutoRegressiveModel):
 
-    def __init__(self, filter_percentile=0, required_matches={}):
-        self.filter_percentile = filter_percentile
+    def __init__(self, filter_first_n=-1, required_matches={}, detrend=False, detrend_poly_degree=3):
+        self.filter_first_n = filter_first_n
         self.required_matches = required_matches
+        self.detrend = detrend
+        self.detrend_poly_degree = detrend_poly_degree
 
     def __str__(self):
         return 'FFT'
 
-
-    def ifft_at(self, n: int):
-        N = len(self.fft_values)
-        arr = np.exp(np.array(range(N)) * 2j * np.pi * n / N)
-        return (np.dot(self.fft_values_filtered, arr) / N).real
+    def chosen_frequencies(self):
+        if (not self._fit_called):
+            raise_log(Exception('fit() must be called before chosen_frequencies()'), logger)
+        frequencies = np.fft.fftfreq(len(self.fft_values))
+        chosen_frequencies = frequencies[self.filtered_indices]
+        return chosen_frequencies
 
     @time_log(logger=logger)
     def fit(self, series: 'TimeSeries'):
         super().fit(series)
 
+        # determine trend
+        if (self.detrend):
+            trend_coefficients = np.polyfit(range(len(self.training_series)), series.values(), 3)
+            self.trend = np.poly1d(trend_coefficients)
+        else:
+            self.trend = lambda x: 0
+
+        # crop training set to match the seasonality of the first prediction point
         self.cropped_series = crop_to_match_seasons(series, required_matches=self.required_matches)
 
-        self.fft_values = np.fft.fft(self.cropped_series.values())
-        threshold = np.percentile(abs(self.fft_values), self.filter_percentile)
+        # subtract trend
+        detrended_values = self.cropped_series.values() - self.trend(range(len(series) - len(self.cropped_series), len(series)))
 
-        def filter_amplitude(x):
-            if (abs(x) < threshold):
-                return 0
-            else:
-                return x
+        # perform dft
+        self.fft_values = np.fft.fft(detrended_values)
 
-        f = np.vectorize(filter_amplitude)
-        self.fft_values_filtered = f(self.fft_values)
+        # filter out low-amplitude frequencies
+        first_n = self.filter_first_n
+        if (first_n < 0 or first_n > len(self.fft_values)): first_n = len(self.fft_values)
+        self.filtered_indices = np.argpartition(abs(self.fft_values), -first_n)[-first_n:]
+        self.fft_values_filtered = np.zeros(len(self.fft_values), dtype=np.complex_)
+        self.fft_values_filtered[self.filtered_indices] = self.fft_values[self.filtered_indices]
+
+        # precompute all possible predicted values
+        self.predicted_values = np.fft.ifft(self.fft_values_filtered).real
 
 
     def predict(self, n: int):
         super().predict(n)
-        first_prediction_index = len(self.fft_values)
-        forecast = np.array([self.ifft_at(i + first_prediction_index) for i in range(n)])
-        return self._build_forecast_series(forecast)
+        trend_forecast = np.array([self.trend(i + len(self.training_series)) for i in range(n)])
+        periodic_forecast = np.array([self.predicted_values[i % len(self.predicted_values)] for i in range(n)])
+        return self._build_forecast_series(periodic_forecast + trend_forecast)
