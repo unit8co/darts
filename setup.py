793e790756ee85232c7779823ebef0636da6f42c
Radu <radu.jica@gmail.com>
2020-04-28 11:33:41 +0200

    add publish requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 requirements = {}
-for extra in ['docs', 'main', 'tests', 'examples']:
+for extra in ['docs', 'main', 'tests', 'examples', 'publish']:
     requirements[extra] = [
         r for r in Path(f'requirements/{extra}.txt').read_text().splitlines()
     ]
