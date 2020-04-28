058b6115d578f8d5e9a606b6185b5f8b29c84276
Radu <radu.jica@gmail.com>
2020-04-28 11:04:52 +0200

    revert to setup.py extras

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-import os
+from pathlib import Path
 
-import yaml
 from setuptools import setup, find_packages
 
-
-def read_requirements_from_conda():
-    path = os.path.join('conda_recipe', 'u8timeseries', 'meta.yaml')
-    with open(path, 'r') as stream:
-        requirements = yaml.safe_load(stream)['requirements']['run']
-        # remove the first python requirement
-        requirements = requirements[1:]
-
-        return requirements
+requirements = {}
+for extra in ['docs', 'main', 'tests', 'examples']:
+    requirements[extra] = [
+        r for r in Path(f'requirements/{extra}.txt').read_text().splitlines()
+    ]
+main_requirements = requirements.pop('main')
 
 
-setup(name='u8timeseries',
-      version=open("u8timeseries/VERSION", "r").read(),
+setup(
+      name='u8timeseries',
+      version=open('u8timeseries/VERSION', 'r').read(),
       description='A collection of easy-to-use timeseries forecasting models',
       url='http://github.com/unit8co/u8timeseries',
       author='Unit8 SA',
       author_email='info@unit8.co',
       license='Apache License 2.0',
       packages=find_packages(),
-      install_requires=read_requirements_from_conda(),
+      install_requires=main_requirements,
+      extras_require=requirements,
       zip_safe=False,
       python_requires='>=3.6',
       package_data={'u8timeseries': ['VERSION']}
-      )
+)
