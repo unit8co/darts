f420cb393de4852265d04034ae772591ba5603e2
Unit8 <unit8@MacBook-Air.local>
2020-04-30 17:20:22 +0200

    fix(rework_imports): reintroduced imports into submodule __init__.py files and added missing ones

---
+++ u8timeseries/backtesting/__init__.py
@@ -0,0 +1,2 @@
+from .backtesting_udf import get_train_val_series, backtest_autoregressive_model
+from .forecasting_simulation import simulate_forecast_ar, simulate_forecast_regr
---
+++ u8timeseries/models/__init__.py
@@ -0,0 +1,7 @@
+from .arima import Arima, AutoArima
+from .baselines import KthValueAgoBaseline
+from .exponential_smoothing import ExponentialSmoothing
+from .prophet import Prophet
+from .standard_regressive_model import StandardRegressiveModel
+from .autoregressive_model import AutoRegressiveModel
+from .theta import Theta
