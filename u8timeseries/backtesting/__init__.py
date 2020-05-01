d5ae46565ebbee0a0892178c127fc0d72becdd08
Julien Herzen <julien.herzen@unit8.co>
2020-05-01 21:38:21 +0200

    Removed legacy backtesting, renamed files

--- u8timeseries/backtesting/__init__.py
+++ u8timeseries/backtesting/__init__.py
@@ -3,5 +3,4 @@ Backtesting
 -----------
 """
 
-from .backtesting_udf import get_train_val_series, backtest_autoregressive_model
-from .forecasting_simulation import backtest_forecasting, backtest_regression
+from .backtesting import backtest_forecasting, backtest_regression
