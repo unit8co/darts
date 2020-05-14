3319fcc6623a6bc87d6444e7bbc886947cf5edb8
pennfranc <flaessig#student.ethz.ch>
2020-05-14 10:08:56 +0200

    feature(residuals): added unit tests

---
+++ u8timeseries/tests/test_backtesting.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+
+from ..backtesting import forecasting_residuals
+from ..models.baselines import NaiveSeasonal
+from ..utils.timeseries_generation import constant_timeseries, linear_timeseries
+
+class BacktestingTestCase(unittest.TestCase):
+
+    def test_forecasting_residuals(self):
+        model = NaiveSeasonal(K=1)
+
+        # test zero residuals
+        constant_ts = constant_timeseries(length=20)
+        residuals = forecasting_residuals(model, constant_ts)
+        np.testing.assert_almost_equal(residuals.values(), np.zeros(len(residuals)))
+
+        # test constant, positive residuals
+        linear_ts = linear_timeseries(length=20)
+        residuals = forecasting_residuals(model, linear_ts)
+        np.testing.assert_almost_equal(np.diff(residuals.values()), np.zeros(len(residuals) - 1))
+        np.testing.assert_array_less(np.zeros(len(residuals)), residuals.values())
+
