7a4df4861e2f368dc482dd104f4d0a300d27275a
pennfranc <flaessig#student.ethz.ch>
2020-05-18 16:46:06 +0200

    feature(backtest_gridsearch): implemented a gridsearch function that finds the optimal parameter combination given a ForecastingModel subclass, a set of possible parameters and a TimeSeries instance

--- u8timeseries/backtesting/backtesting.py
+++ u8timeseries/backtesting/backtesting.py
@@ -5,13 +5,14 @@ Backtesting Functions
 
 import pandas as pd
 import numpy as np
-from u8timeseries.timeseries import TimeSeries
-from u8timeseries.models.forecasting_model import ForecastingModel
-from u8timeseries.models.regression_model import RegressionModel
-
-from u8timeseries.utils import _build_tqdm_iterator
+from ..timeseries import TimeSeries
+from ..models.forecasting_model import ForecastingModel
+from ..models.regression_model import RegressionModel
+from .. import metrics
+from ..utils import _build_tqdm_iterator
 from ..logging import raise_if_not, get_logger
 from typing import Iterable
+from itertools import product
 
 logger = get_logger(__name__)
 
@@ -166,3 +167,59 @@ def backtest_regression(feature_series: Iterable[TimeSeries],
         times.append(pred.end_time())  # store the N-th timestamp
 
     return TimeSeries.from_times_and_values(pd.DatetimeIndex(times), np.array(values))
+
+
+def backtest_gridsearch(model_class: type, parameters: dict, series: TimeSeries, fcast_horizon_n: int,
+                        num_predictions: int = 10, metric='mape', verbose=False):
+    """ A function for finding the best hyperparameters.
+    
+    Computes 'num_predictions' predictions with horizon 'fcast_horizon_n' for every combination
+    of hyperparameter values provided in the 'parameters' dictionary using an instance of the 
+    given 'model_class' subclass of ForecastingModel. An instance with the best-performing
+    hyperparameters (with respect to 'metric') will be returned.
+
+    Parameters
+    ----------
+    model
+        A ForecastingModel subclass to be tuned for 'series'.
+    parameters
+        A dictionary containg as keys hyperparameter names, and as values lists of values for the
+        respective hyperparameter.
+    series
+        The TimeSeries instance used for backtesting.
+    fcast_horizon_n
+        The integer value of the forecasting horizon used during backtesting.
+    num_predictions:
+        The number of train/predictions cycles performed when testing one hyperparameter combination.
+    metric:
+        The function name (as string) of a metrics function from the metrics module.
+    verbose:
+        Whether to print progress.
+    
+    Returns
+    -------
+    ForecastingModel
+        A 'model_cls' instance with the best-performing hyperparameters from the given selection.
+    """
+
+    raise_if_not(hasattr(metrics, metric), "'metric' must be an attribute of u8timeseries.metriecs.", logger)
+
+    backtest_start_time = series.end_time() - (num_predictions + fcast_horizon_n) * series.freq()
+    metric_function = getattr(metrics, metric)
+
+    min_error = float('inf')
+    best_param_combination = {}
+    params_cross_product = list(product(*parameters.values()))
+    iterator = _build_tqdm_iterator(params_cross_product, verbose)
+
+    # iterate through all combinations of the provided parameters and choose the best one
+    for param_combination in iterator:
+        param_combination_dict = dict(list(zip(parameters.keys(), param_combination)))
+        model = model_class(**param_combination_dict)
+        backtest_forecast = backtest_forecasting(series, model, backtest_start_time, fcast_horizon_n)
+        error = metric_function(backtest_forecast, series)
+        if (error < min_error):
+            min_error = error
+            best_param_combination = param_combination_dict
+    logger.info('Chosen parameters: ' + str(best_param_combination))
+    return model_class(**best_param_combination)
