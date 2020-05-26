776a439a758031b3ebf4a58b65176e88d81ebd70
pennfranc <flaessig#student.ethz.ch>
2020-05-26 17:23:02 +0200

    fix(testing): fixed TCN fit test

--- u8timeseries/tests/test_TCN.py
+++ u8timeseries/tests/test_TCN.py
@@ -2,6 +2,7 @@ import shutil
 import unittest
 
 import pandas as pd
+import logging
 
 from ..models.tcn_model import TCNModel
 from ..timeseries import TimeSeries
@@ -11,20 +12,27 @@ from ..metrics import mape
 
 class TCNModelTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        logging.disable(logging.CRITICAL)
+
     def test_creation(self):
         with self.assertRaises(ValueError):
             # cannot choose a kernel size larger than the input length
-            TCNModel(kernel_size=100, input_length=50)
+            TCNModel(kernel_size=100, input_length=20)
         model = TCNModel()
     
     def test_fit(self):
-        sine_ts = tg.sine_timeseries(length=100)
+        large_ts = tg.constant_timeseries(length=100, value=1000)
+        small_ts = tg.constant_timeseries(length=100, value=10)
 
-        # Test basic fit
-        model1 = TCNModel(n_epochs=2)
-        model1.fit(sine_ts[:80])
+        # Test basic fit and predict
+        model = TCNModel(n_epochs=20, num_layers=1)
+        model.fit(large_ts[:98])
+        pred = model.predict(n=2).values()[0]
 
-        # Test model improvement
-        model2 = TCNModel(n_epochs=4)
-        model2.fit(sine_ts[:80])
-        self.assertTrue(mape(model2.predict(n=20), sine_ts[80:]) <= mape(model1.predict(n=20), sine_ts[80:]))
+        # Test whether model trained on one series is better than one trained on another
+        model2 = TCNModel(n_epochs=20, num_layers=1)
+        model2.fit(small_ts[:98])
+        pred2 = model2.predict(n=2).values()[0]
+        self.assertTrue(abs(pred2 - 10) < abs(pred - 10))
