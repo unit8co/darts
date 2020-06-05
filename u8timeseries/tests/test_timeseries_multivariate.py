b3445a44fe4caf93c67af695db51b57328fa9a06
pennfranc <flaessig#student.ethz.ch>
2020-06-05 16:06:06 +0200

    feature(tests): added separate multivariate test file for TimeSeries which contains some tests for already existing TimeSeries methods

---
+++ u8timeseries/tests/test_timeseries_multivariate.py
@@ -0,0 +1,76 @@
+import logging
+import unittest
+import math
+
+import numpy as np
+import pandas as pd
+
+from ..timeseries import TimeSeries
+
+
+class TimeSeriesTestCase(unittest.TestCase):
+
+    times = pd.date_range('20130101', '20130110')
+    dataframe1 = pd.DataFrame({
+        0: range(10),
+        1: range(5, 15),
+        2: range(10, 20)
+    }, index=times)
+    dataframe2 = pd.DataFrame({
+        0: np.arange(1, 11),
+        1: np.arange(1, 11) * 3,
+        2: np.arange(1, 11) * 5
+    }, index=times)
+    series1 = TimeSeries(dataframe1)
+    series2 = TimeSeries(dataframe2)
+
+    @classmethod
+    def setUpClass(cls):
+        logging.disable(logging.CRITICAL)
+
+    def test_creation(self):
+        with self.assertRaises(ValueError):
+            # Index is dateTimeIndex
+            TimeSeries(pd.DataFrame(range(10), range(10)))
+        series_test = TimeSeries(self.dataframe1)
+        self.assertTrue(series_test.pd_dataframe().equals(self.dataframe1))
+
+        # Series cannot be lower than three without passing frequency as argument to constructor
+        index = pd.date_range('20130101', '20130102')
+        with self.assertRaises(ValueError):
+            TimeSeries(self.dataframe1.iloc[:2, :])
+        TimeSeries(self.dataframe1.iloc[:2, :], 'D')
+
+    def test_eq(self):
+        seriesA = TimeSeries(self.dataframe1)
+        self.assertTrue(self.series1 == seriesA)
+        self.assertFalse(self.series1 != seriesA)
+
+        # with different dates
+        dataframeB = self.dataframe1.copy()
+        dataframeB.index = pd.date_range('20130102', '20130111')
+        seriesB = TimeSeries(dataframeB)
+        self.assertFalse(self.series1 == seriesB)
+
+        # with one different value
+        dataframeC = self.dataframe1.copy()
+        dataframeC.iloc[2, 2] = 0
+        seriesC = TimeSeries(dataframeC)
+        self.assertFalse(self.series1 == seriesC)
+
+    def test_rescale(self):
+        with self.assertRaises(ValueError):
+            self.series1.rescale_with_value(1)
+
+        seriesA = self.series2.rescale_with_value(0)
+        self.assertTrue(np.all(seriesA.values() == 0).all())
+
+        seriesB = self.series2.rescale_with_value(1)
+        self.assertEqual(seriesB, TimeSeries(
+            pd.DataFrame({
+                0: np.arange(1, 11),
+                1: np.arange(1, 11),
+                2: np.arange(1, 11)
+            }, index=self.dataframe2.index).astype(float)
+        ))
+        
