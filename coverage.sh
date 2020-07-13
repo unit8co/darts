4f4888f6408d4701582a0d8fc4e7de232399f2bf
Guillaume Raille <guillaume.raille@unit8.co>
2020-07-13 14:45:54 +0200

    improve coverage sh script

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,18 @@
-# run and display coverage report properly filtered
+#!/bin/bash
+# run and display coverage report properly filtered in terminal, xml or html
+usage()
+{
+    echo "usage: ./coverage.sh [ xml | html ]"
+}
+
+#### Main
 coverage run --source=. -m unittest
-coverage report -m --fail-under=80 --omit='darts/tests*,*__init__.py'
+if [ -z "$1" ]; then
+    coverage report -m --fail-under=80 --omit='darts/tests*,*__init__.py'
+else
+    case $1 in
+        xml )  coverage xml --fail-under=80 --omit='darts/tests*,*__init__.py' ;;
+        html ) coverage html --fail-under=80 --omit='darts/tests*,*__init__.py' ;;
+        * )    usage; exit 1;;
+    esac
+fi
