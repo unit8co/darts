186dbc87dd55662bd3732d70c50f6fd837f3ec8a
Droxef <frederick@unit8.co>
2020-07-23 17:05:03 +0200

    perf(ts_creation): create index only one time per freq and slice it

--- examples/M4_competition/create_ts.py
+++ examples/M4_competition/create_ts.py
@@ -21,34 +21,34 @@ info_dataset = pd.read_csv('./dataset/M4-info.csv', delimiter=',').set_index('M4
 
 # creating time series
 for i, dc in _build_tqdm_iterator(enumerate(data_categories), verbose=True):
+    if os.path.isfile("dataset/train_"+dc+".pkl") and os.path.isfile("dataset/test_"+dc+".pkl"):
+        print(" TimeSeries already created")
+        continue
     train_set = train_datasets[i]
     test_set = test_datasets[i]
     ts_train = []
     ts_test = []
-    if os.path.isfile("dataset/train_"+dc+".pkl") and os.path.isfile("dataset/test_"+dc+".pkl"):
-        print(" TimeSeries already created")
-        continue
+    forecast_horizon = test_set.shape[0]
+    if dc == 'Yearly':
+        index = pd.date_range(pd.Timestamp.min,
+                              periods=584,
+                              freq=info_dataset.SP.str[0][dc[0] + '1'])
+        fallback_index = pd.date_range(pd.Timestamp.min,
+                                       periods=train_set.shape[0] + forecast_horizon,
+                                       freq='Q')
+    else:
+        index = pd.date_range(pd.Timestamp.min,
+                              periods=train_set.shape[0] + forecast_horizon,
+                              freq=info_dataset.SP.str[0][dc[0]+'1'])
     for ts in _build_tqdm_iterator(train_set.columns, verbose=True):
-        if dc == 'Yearly' and train_set.count()[ts] > 490:
-            print("ts too big, fallback to quaterly frequency")
-            info_dataset.SP[ts] = "Quarterly"
-        try:
-            train_index = pd.date_range(info_dataset.StartingDate[ts],
-                                        periods=train_set.count()[ts],
-                                        freq=info_dataset.SP.str[0][ts])
-            series_train = TimeSeries.from_series(train_set[ts].dropna().set_axis(train_index))
-            test_index = pd.date_range(series_train.time_index()[-1],
-                                       periods=info_dataset.Horizon[ts]+1,
-                                       freq=info_dataset.SP.str[0][ts])
-        except pd.errors.OutOfBoundsDatetime:
-            train_index = pd.date_range(info_dataset.StartingDate[ts][:6] + '17' + info_dataset.StartingDate[ts][6:],
-                                        periods=train_set.count()[ts],
-                                        freq=info_dataset.SP.str[0][ts])
-            series_train = TimeSeries.from_series(train_set[ts].dropna().set_axis(train_index))
-            test_index = pd.date_range(series_train.time_index()[-1],
-                                       periods=info_dataset.Horizon[ts]+1,
-                                       freq=info_dataset.SP.str[0][ts])
-        series_test = TimeSeries.from_series(test_set[ts].dropna().set_axis(test_index[1:]))
+        train_index = index
+        if dc == 'Yearly' and train_set.count()[ts] > (len(index) - forecast_horizon):
+            print("ts too big, fallback to quarterly frequency")
+            train_index = fallback_index
+        series_train = TimeSeries.from_series(train_set[ts].dropna().set_axis(train_index[:train_set.count()[ts]]))
+        series_test = TimeSeries.from_series(test_set[ts].dropna().
+                                             set_axis(train_index[train_set.count()[ts]:
+                                                                  train_set.count()[ts]+forecast_horizon]))
         ts_train.append(series_train)
         ts_test.append(series_test)
     pickle.dump(ts_train, open("dataset/train_"+dc+".pkl", "wb"))
