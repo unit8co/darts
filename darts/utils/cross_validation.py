c1c93e57640166d0ffcff71ac8e66bc16c509896
Droxef <frederick@unit8.co>
2020-08-05 13:39:38 +0200

    fix(groe): correct incorrect behavior and documentation

--- darts/utils/cross_validation.py
+++ darts/utils/cross_validation.py
@@ -5,7 +5,7 @@ Cross-validation functions
 
 from ..timeseries import TimeSeries
 from ..metrics import metrics as mfunc
-from ..logging import get_logger, raise_if_not
+from ..logging import get_logger, raise_if_not, raise_log
 from typing import Union, Callable, Optional
 import numpy as np
 
@@ -29,9 +29,10 @@ def generalized_rolling_origin_evaluation(ts: TimeSeries, model: ForecastingMode
     If `stride = 1`, the execution is similar to a Rolling Origin Evaluation.
     If `stride >= len(ts) - origin1` the execution is similar to a Fixed Origin Evaluation.
 
-    At least two parameters from `stride`, `n_evaluation` and `n_prediction` must be given.
+    At least one parameter from `stride` and `n_evaluation` must be given.
 
     If ValueErrors occur, the function will return `np.inf`.
+    TODO: origin1 could be a TimeStamp
 
     Parameters
     ----------
@@ -43,33 +44,40 @@ def generalized_rolling_origin_evaluation(ts: TimeSeries, model: ForecastingMode
         The metrics to use. Either a function from taking 2 TimeSeries as parameters,
         or a string of the name of the function from darts.metrics.
     origin1
-        The index of the first origin. Defaults is the minimum between len(ts) - 10 and 5.
+        Optional. The index of the first origin. Defaults is the minimum between len(ts) - 10 and 5.
     stride
-        The stride used for rolling the origin. Defaults is n_prediction / n_evaluation if provided.
+        Optional. The stride used for rolling the origin. Defaults is n_prediction / n_evaluation if provided.
     n_evaluation
-        Number of evaluation. Defaults is the maximum number possible.
+        Optional. Number of evaluation. Defaults is the maximum number possible if stride is provided.
     n_prediction
-        Number of predictions for each evaluation. Defaults is len(ts) - origin1.
+        Optional. Number of predictions for each evaluation. Defaults is the size of the tail: len(ts) - origin1.
     Returns
     -------
     Float
         The sum of the predictions errors over the different origins.
     """
-    raise_if_not(((stride is None) + (n_prediction is None) + (n_evaluation is None)) > 1,
-                 "At least 2 parameters between stride, n_prediction and n_evaluation must be given")
+    raise_if_not((stride is not None) or (n_evaluation is not None),
+                 "At least 1 parameter between stride and n_evaluation must be given")
     raise_if_not(callable(metrics) or hasattr(mfunc, metrics),
-                 "The metrics should be a function from darts.metrics or a string of its name")
+                 "The metrics should be a function that takes TimeSeries as inputs,"
+                 " or a string of a function name from darts.metrics")
     if type(metrics) is str and metrics != 'mase':
-        metrics = getattr(mfunc, metrics)
+        try:
+            metrics = getattr(mfunc, metrics)
+        except ValueError:
+            raise_log(ValueError("The metrics should be a function that takes TimeSeries as inputs,"
+                      " or a string of a function name from darts.metrics"), logger)
     len_ts = len(ts)
     if origin1 is None:
         origin1 = max(5, len_ts - 10)
-    if stride is None:
-        stride = int(np.floor(n_prediction / n_evaluation))
+    elif origin1 >= len_ts or origin1 <= 0:
+        raise_log(ValueError("origin1 must be inside the TimeSeries"), logger)
     if n_prediction is None:
         n_prediction = len_ts - origin1
     if n_evaluation is None:
-        n_evaluation = int(1 + np.floor(n_prediction / stride))
+        n_evaluation = int(1 + np.floor((len_ts - origin1) / stride))
+    elif stride is None:
+        stride = int(np.floor(n_prediction / n_evaluation))
     errors = []
     for i in range(n_evaluation):
         # if origin is further than end timestamp, end function
@@ -90,9 +98,9 @@ def generalized_rolling_origin_evaluation(ts: TimeSeries, model: ForecastingMode
             continue
         try:
             if metrics == 'mase':
-                error = getattr(mfunc, metrics)(test, forecast, train) * n_evaluation
+                error = getattr(mfunc, metrics)(test, forecast, train) * n_pred
             else:
-                error = metrics(test, forecast) * n_evaluation
+                error = metrics(test, forecast) * n_pred
             errors.append(error)
         except ValueError:
             # if cannot use metrics, return np.inf
