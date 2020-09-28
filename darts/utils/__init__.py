59bc50d62da9b92e2f46ca176112c3ed4056718b
Tafti Léo <43440848+LeoTafti@users.noreply.github.com>
2020-09-28 10:39:42 +0200

    Refactor/backtest checks (#189)
    * fix(backtest): Fixed issue with sanity checks when using forecast_horizon default value
    
    * fix(style): linter
    
    * refactor(backtest): Improved RegressionModel.backtest() start parameter type and factored out checks shared by forecasting and regression models
    
    * fix(backtest): Fixed error with 0 or small (float) value of backtest() start param
    
    Also fixed style (linter), added a couple unit tests, and set a smarter default value for start in StandardRegressionModel
    
    * fix(backtest): Fixed error with 0 or small (float) value of backtest() start param
    
    Also fixed style (linter), added a couple unit tests, and set a smarter default value for start in StandardRegressionModel
    
    * fix(style): lint
    
    * fix(style): lint
    
    * fix(backtest): Fixed issue with kwargs being passed as positional arguments
    
    * fix(style): better comments
    
    * fix(style): better doc and variable names
    
    * fix: implemented suggestions from review
    
    Co-authored-by: Léo Tafti Unit8 <leotaftiu8@Leos-MacBook-Pro.local>

--- darts/utils/__init__.py
+++ darts/utils/__init__.py
@@ -2,4 +2,4 @@
 Utils
 -----
 """
-from .utils import retain_period_common_to_all, _build_tqdm_iterator, _with_sanity_checks
+from .utils import retain_period_common_to_all, _build_tqdm_iterator, _with_sanity_checks, _backtest_general_checks, _get_timestamp_at_point
