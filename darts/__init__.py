fe5efc1b96c2aa623864a490e8f9efe8f1be8c2c
Léo Tafti <leotafti@gmail.com>
2020-09-28 09:43:47 +0200

    fix(mplstyle error): Removed .mplstyle file and replaced with dynamic styling. Works from everywhere. Re-ran notebooks

--- darts/__init__.py
+++ darts/__init__.py
@@ -4,7 +4,9 @@ darts
 """
 
 from .timeseries import TimeSeries
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib import cycler
 
 # Enums
 from enum import Enum
@@ -28,4 +30,25 @@ class ModelMode(Enum):
 
 __version__ = 'dev'
 
-plt.style.use('../darts/u8plots.mplstyle')
+colors = cycler(color=['black', '003DFD', 'b512b8', '11a9ba', '0d780f', 'f77f07', 'ba0f0f'])
+
+u8plots_mplstyle = {
+    'font.family' : 'Arial',
+    'figure.facecolor' : '#f0f0f0',
+    'axes.facecolor' : '#f0f0f0',
+    'axes.prop_cycle' : colors,
+    'lines.linewidth' : 1.3,
+    'axes.spines.top' : False,
+    'axes.spines.right' : False,
+    'axes.spines.bottom' : False,
+    'axes.spines.left' : False,
+    'axes.linewidth' : 1,
+    'axes.edgecolor' : 'black',
+    'xtick.bottom' : False,
+    'ytick.left' : False,
+    'axes.grid' : True,
+    'grid.color' : '#d0d0d0',
+    'grid.alpha' : 0.5,
+    'legend.frameon' : False}
+
+mpl.rcParams.update(u8plots_mplstyle)
