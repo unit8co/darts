1c99fb6b8c600896620c3bfd332815b35ee78219
Digma <gael.grosch@unit8.co>
2020-10-27 14:06:48 +0100

    feat(plot): change matplotlib default style

--- darts/__init__.py
+++ darts/__init__.py
@@ -33,22 +33,27 @@ __version__ = 'dev'
 colors = cycler(color=['black', '003DFD', 'b512b8', '11a9ba', '0d780f', 'f77f07', 'ba0f0f'])
 
 u8plots_mplstyle = {
-    'font.family' : 'Arial',
-    'figure.facecolor' : '#f0f0f0',
-    'axes.facecolor' : '#f0f0f0',
+    'font.family' : 'sans serif',
+    'axes.edgecolor' : 'black',
+    'axes.grid' : True,
+    'axes.labelcolor': '#333333',
+    'axes.labelweight' : 600,
+    'axes.linewidth' : 1,
     'axes.prop_cycle' : colors,
-    'lines.linewidth' : 1.3,
     'axes.spines.top' : False,
     'axes.spines.right' : False,
     'axes.spines.bottom' : False,
     'axes.spines.left' : False,
-    'axes.linewidth' : 1,
-    'axes.edgecolor' : 'black',
-    'xtick.bottom' : False,
-    'ytick.left' : False,
-    'axes.grid' : True,
     'grid.color' : '#d0d0d0',
-    'grid.alpha' : 0.5,
-    'legend.frameon' : False}
+    'legend.frameon' : False,
+    'lines.linewidth' : 1.3,
+    'xtick.bottom' : False,
+    'xtick.color': '#333333',
+    'xtick.labelsize':'small',
+    'ytick.color': '#333333',
+    'ytick.labelsize':'small',
+    'xtick.bottom' : False,
+}
+
 
 mpl.rcParams.update(u8plots_mplstyle)
