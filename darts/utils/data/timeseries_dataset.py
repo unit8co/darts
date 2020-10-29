72e59097356608e9e3e15a9f413e1f81d2ca7148
Julien Herzen <julien@unit8.co>
2020-10-29 15:10:26 +0100

    time series dataset

--- darts/utils/data/timeseries_dataset.py
+++ darts/utils/data/timeseries_dataset.py
@@ -1,14 +1,21 @@
-from torch.utils.data import Dataset
 from typing import Union, Optional, Sequence, Tuple
-from ...logging import raise_if_not, raise_if, get_logger
-
+from ...logging import raise_if_not, get_logger
 from ...timeseries import TimeSeries
+
+logger = get_logger(__name__)
+
+try:
+    import torch
+    from torch.utils.data import Dataset
+except ModuleNotFoundError:
+    logger.warning("Support Torch based models not available. To enable it, install u8darts[torch] or u8darts[all].")
 
 
 class TimeSeriesDataset(Dataset):
     def __init__(self,
-                 series: Union[TimeSeries, Sequence[TimeSeries]],
                  horizon: int,
+                 data_series: Union[TimeSeries, Sequence[TimeSeries]],
+                 target_series: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
                  lh: Tuple[int] = (1, 3),
                  lookback: Tuple[int] = (2, 7)) -> None:
         """
@@ -29,15 +36,20 @@ class TimeSeriesDataset(Dataset):
         of coming from any of the N time series in the sequence, and so depending on the parameters,
         the same (data, target) sample may be sampled more often if it belongs to shorter time series.
         Note: all (data, target) samples will be sampled with the same frequency only if all the time series in
-        the provided sequence have length at least `(max_lb + max_lh) * H`.
+        the provided sequences have length at least `(max_lb + max_lh) * H`.
 
         The recommended use of this class is to either build it from a list of TimeSeries (if all your series fit
         in memory), or implement your own Sequence of time series (i.e., re-implement `__len__()` and `__getitem__()`)
         and give such an instance as argument to this class.
 
-        :param series: one or a sequence of TimeSeries
         :param horizon: The horizon `H` of the underlying model being trained. The emitted targets
                         will have length (up to) `H`.
+        :param data_series: one or a sequence of `TimeSeries` containing the "features" dimensions. These
+                            are the dimensions that will be accessed by the model to make forecasts.
+        :param target_series: optionally, one or a sequence of `TimeSeries` containing the "target" dimensions.
+                              These are the dimensions that will be forecast by the model.
+                              If this parameter is not set, the model will predict the dimensions of `data_series`.
+                              If it is set, the provided sequence must have the same length as that of `data_series`.
         :param lh: A `(min_lh, max_lh)` interval for the forecast point, starting from the end of the series.
                    For example, `(1, 3)` will select forecast points uniformly between `1*H` and `3*H` points
                    before the end of the series.
@@ -49,7 +61,11 @@ class TimeSeriesDataset(Dataset):
                          on the maximum size allowed by the series' length and the selection of the forecast point.
         """
 
-        self.series = [series] if isinstance(series, TimeSeries) else series
+        self.data_series = [data_series] if isinstance(data_series, TimeSeries) else data_series
+        if target_series is None:
+            self.target_series = data_series
+        else:
+            self.target_series = [target_series] if isinstance(target_series, TimeSeries) else target_series
         self.H = horizon
         self.min_lh, self.max_lh = lh
         self.min_lb, self.max_lb = lookback
@@ -59,8 +75,12 @@ class TimeSeriesDataset(Dataset):
                      'The lh parameter should be an int tuple (min_lh, max_lh), with min_lh <= max_lh')
         raise_if_not(self.min_lb <= self.max_lh,
                      'The lookback parameter should be an int tuple (min_lb, max_lb), with min_lb <= max_lb')
+        raise_if_not(len(self.data_series) == len(self.target_series),
+                     'The provided sequence of target series must have the same length as '
+                     'the provided sequence of data series.')
 
-        self.ideal_nr_samples = len(self.series) * (self.max_lh - self.min_lh)*self.H * (self.max_lb - self.min_lb)
+        self.nr_samples_per_ts = (self.max_lh - self.min_lh)*self.H * (self.max_lb - self.min_lb)
+        self.ideal_nr_samples = len(self.data_series) * self.nr_samples_per_ts
 
     def __len__(self):
         """
@@ -71,4 +91,28 @@ class TimeSeriesDataset(Dataset):
         return self.ideal_nr_samples
 
     def __getitem__(self, idx):
-        pass
+        ts_idx = idx // self.nr_samples_per_ts  # index of the time series
+        lh_lb_idx = idx - (ts_idx * len(self.data_series))  # index of the (data, target) split within the series
+
+        lh_idx = lh_lb_idx // (self.max_lb - self.min_lb)  # index of the actual l_h, determining the forecasting point
+        lb_idx = lh_lb_idx - (lh_idx * (self.max_lh - self.min_lh)*self.H)  # index of which lookback period to use
+        assert lh_idx < (self.max_lh - self.min_lb) * self.H, 'bug in the Lh indexing'
+        assert lb_idx <= (self.max_lb - self.min_lb), 'bug in lb indexing'
+
+        # select time series
+        ts_data = self.data_series[ts_idx]
+        ts_target = self.target_series[ts_idx]
+
+        # select forecast point and target period
+        ts_target_values = ts_target.values(copy=False)
+        forecast_point_idx = self.min_lb * self.H + lh_idx  # index
+        target_values = ts_target_values[-forecast_point_idx:-forecast_point_idx+self.H]
+        fc_point_ts = ts_target.time_index()[-forecast_point_idx]  # timestamp
+
+        # select data period; look at the `lookback * H` points before the *timestamp* of the forecast point
+        data_series_upto_fc_point = ts_data[:fc_point_ts]
+        ts_data_values = data_series_upto_fc_point.values(copy=False)
+        nr_lookback_points = (self.min_lb + lb_idx) * self.H
+        data_values = ts_data_values[-nr_lookback_points:]
+
+        return torch.from_numpy(data_values).float(), torch.from_numpy(target_values).float()
