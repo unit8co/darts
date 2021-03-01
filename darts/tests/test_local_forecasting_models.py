025f6b560eeadf6f756e50c1dbd72b6bd52a9b6e
Thomas <neuer.thom@gmail.com>
2021-03-01 15:39:58 +0100

    Update documentation

--- darts/tests/test_local_forecasting_models.py
+++ darts/tests/test_local_forecasting_models.py
@@ -72,7 +72,7 @@ class LocalForecastingModelsTestCase(DartsBaseTestClass):
     ts_passengers = TimeSeries.from_dataframe(df, 'Month', ['#Passengers'])
     ts_pass_train, ts_pass_val = ts_passengers.split_after(pd.Timestamp('19570101'))
 
-    # real timeseries for functionality tests
+    # real multivariate timeseries for functionality tests
     multivariate_df = pd.read_csv('examples/ice_cream_heater.csv', delimiter=",")
     ts_ice_heater = TimeSeries.from_dataframe(multivariate_df, 'Month', ['ice cream', 'heater'])
     ts_ice_heater_train, ts_ice_heater_val = ts_ice_heater.split_after(split_point=0.7)
