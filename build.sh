c2ea217f0a619d75b775df9f5214e891a69e3558
Julien Herzen <julien@unit8.co>
2021-03-25 11:07:22 +0100

    add build script and modify readme

---
+++ build.sh
@@ -0,0 +1,10 @@
+python -m pip install --upgrade build
+
+# build darts
+python -m build
+
+# build u8darts, swapping the setup.py files temporarily
+cp setup.py setup_darts.py
+cp setup_u8darts.py setup.py
+python -m build
+mv setup_darts.py setup.py
