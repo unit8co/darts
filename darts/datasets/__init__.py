cea1aa1b44601924945a3201341b131d929c824d
Thomas <neuer.thom@gmail.com>
2021-05-11 08:49:55 +0200

    Update path to datasets

--- darts/datasets/__init__.py
+++ darts/datasets/__init__.py
@@ -6,7 +6,7 @@ from .dataset_loaders import DatasetLoaderCSV, DatasetLoaderMetadata
     ts: TimeSeries = AirPassengersDataset.load()
 """
 
-_DEFAULT_PATH = "https://raw.githubusercontent.com/unit8co/darts/feat/datasets"
+_DEFAULT_PATH = "https://raw.githubusercontent.com/unit8co/darts/feat/datasets/datasets"
 
 AirPassengersDataset = DatasetLoaderCSV(
     metadata=DatasetLoaderMetadata(
