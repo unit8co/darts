0ad77777ef5dcc3270a036397486b655fbe3f66d
hrzn <hrzn@users.noreply.github.com>
2021-05-21 08:02:10 +0000

    Release 0.8.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../..'))
 project = 'darts'
 copyright = '2021, Unit8 SA'
 author = 'Unit8 SA'
-version = '0.7.0'
+version = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
