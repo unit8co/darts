5c156eadea1d36e453c11ae8be24e6fd01976782
Michal Kosinski <michal.kosinski@unit8.co>
2021-06-18 15:14:28 +0200

    [WIP] initial commit, not working yet

--- darts/models/torch_forecasting_model.py
+++ darts/models/torch_forecasting_model.py
@@ -9,6 +9,8 @@ import os
 import re
 from glob import glob
 import shutil
+from collections import defaultdict
+import inspect
 from joblib import Parallel, delayed
 from typing import Optional, Dict, Tuple, Union, Sequence
 from abc import ABC, abstractmethod
@@ -199,6 +201,122 @@ class TorchForecastingModel(GlobalForecastingModel, ABC):
         self.optimizer_kwargs = dict() if optimizer_kwargs is None else optimizer_kwargs
         self.lr_scheduler_cls = lr_scheduler_cls
         self.lr_scheduler_kwargs = dict() if lr_scheduler_kwargs is None else lr_scheduler_kwargs
+
+        if os.path.exists(_get_checkpoint_folder(work_dir, model_name)):
+
+            params = self.get_params()
+            self.load(_get_checkpoint_folder(work_dir, model_name)) # TODO: add checkpoint filename
+            self.set_params(**params)
+
+    def load(self, filename):
+        f = open(filename, 'rb')
+        tmp_dict = torch.load(f)
+        f.close()
+
+        self.__dict__.update(tmp_dict.__dict__)
+
+    @classmethod
+    def _get_param_names(cls):
+        """Get parameter names for the estimator"""
+        # fetch the constructor or the original constructor before
+        # deprecation wrapping if any
+        # source: scikit-learn's base.py
+        init = getattr(cls.__init__, 'deprecated_original', cls.__init__)
+        if init is object.__init__:
+            # No explicit constructor to introspect
+            return []
+
+        # introspect the constructor arguments to find the model parameters
+        # to represent
+        init_signature = inspect.signature(init)
+        # Consider the constructor parameters excluding 'self'
+        parameters = [p for p in init_signature.parameters.values()
+                      if p.name != 'self' and p.kind != p.VAR_KEYWORD]
+        for p in parameters:
+            if p.kind == p.VAR_POSITIONAL:
+                raise RuntimeError("scikit-learn estimators should always "
+                                   "specify their parameters in the signature"
+                                   " of their __init__ (no varargs)."
+                                   " %s with constructor %s doesn't "
+                                   " follow this convention."
+                                   % (cls, init_signature))
+        # Extract and sort argument names excluding 'self'
+        return sorted([p.name for p in parameters])
+
+    def get_params(self, deep=True):
+        """
+        Get parameters for this estimator.
+        source: scikit-learn's base.py
+        Parameters
+        ----------
+        deep : bool, default=Tru
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+        Returns
+        -------
+        params : dict
+            Parameter names mapped to their values.
+        """
+        out = dict()
+        for key in self._get_param_names():
+            value = getattr(self, key)
+            if deep and hasattr(value, 'get_params'):
+                deep_items = value.get_params().items()
+                out.update((key + '__' + k, val) for k, val in deep_items)
+            out[key] = value
+        return out
+
+    def set_params(self, **params):
+        """
+        Set the parameters of this estimator.
+        The method works on simple estimators as well as on nested objects
+        (such as :class:`~sklearn.pipeline.Pipeline`). The latter have
+        parameters of the form ``<component>__<parameter>`` so that it's
+        possible to update each component of a nested object.
+        source: scikit-learn's base.py
+        Parameters
+        ----------
+        **params : dict
+            Estimator parameters.
+        Returns
+        -------
+        self : estimator instance
+            Estimator instance.
+        """
+        if not params:
+            # Simple optimization to gain speed (inspect is slow)
+            return self
+        valid_params = self.get_params(deep=True)
+
+        nested_params = defaultdict(dict)  # grouped by prefix
+        for key, value in params.items():
+            key, delim, sub_key = key.partition('__')
+            if key not in valid_params:
+                raise ValueError('Invalid parameter %s for estimator %s. '
+                                 'Check the list of available parameters '
+                                 'with `estimator.get_params().keys()`.' %
+                                 (key, self))
+
+            if delim:
+                nested_params[key][sub_key] = value
+            else:
+                setattr(self, key, value)
+                valid_params[key] = value
+
+        for key, sub_params in nested_params.items():
+            valid_params[key].set_params(**sub_params)
+
+        return self
+
+    def reset_model(self):
+        shutil.rmtree(_get_checkpoint_folder(self.work_dir, self.model_name), ignore_errors=True)
+        shutil.rmtree(_get_runs_folder(self.work_dir, self.model_name), ignore_errors=True)
+        shutil.rmtree(_get_untrained_models_folder(self.work_dir, self.model_name), ignore_errors=True)
+
+        self.total_epochs = 0
+        self.model = None
+        self.input_dim = None
+        self.output_dim = None
 
     def _init_model(self) -> None:
         """
