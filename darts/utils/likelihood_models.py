f1003a8db0eb29ca40df2ca8066c1a1ef846f5c3
pennfranc <flaessig@ethz.student.ch>
2021-07-02 17:14:18 +0200

    feat(DeepAR): renamed output argument

--- darts/utils/likelihood_models.py
+++ darts/utils/likelihood_models.py
@@ -21,18 +21,18 @@ class LikelihoodModel(ABC):
         pass
 
     @abstractmethod
-    def _compute_loss(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    def _compute_loss(self, model_output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """
-        Computes a loss from a model `output`, which represents the parameters of a given probability
+        Computes a loss from a `model_output`, which represents the parameters of a given probability
         distribution for every ground truth value in `target`, and the `target` itself.
         """
         pass
 
     @abstractmethod
-    def _sample(self, output: torch.Tensor) -> torch.Tensor:
+    def _sample(self, model_output: torch.Tensor) -> torch.Tensor:
         """
         Samples a prediction from the probability distributions defined by the specific likelihood model
-        and the parameters given in `output`.
+        and the parameters given in `model_output`.
         """
         pass
 
@@ -51,21 +51,21 @@ class GaussianLikelihoodModel(LikelihoodModel):
     def __init__(self):
         self.loss = nn.GaussianNLLLoss(reduction='mean')
 
-    def _compute_loss(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        output_means, output_vars = self._means_and_vars_from_output(output)
-        return self.loss(output_means, target, output_vars)
+    def _compute_loss(self, model_output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        model_output_means, model_output_vars = self._means_and_vars_from_model_output(model_output)
+        return self.loss(model_output_means, target, model_output_vars)
 
-    def _sample(self, output: torch.Tensor) -> torch.Tensor:
-        output_means, output_vars = self._means_and_vars_from_output(output)
-        return torch.normal(output_means, output_vars)
+    def _sample(self, model_output: torch.Tensor) -> torch.Tensor:
+        model_output_means, model_output_vars = self._means_and_vars_from_model_output(model_output)
+        return torch.normal(model_output_means, model_output_vars)
 
     @property
     def _num_parameters(self) -> int:
         return 2
 
-    def _means_and_vars_from_output(self, output):
+    def _means_and_vars_from_model_output(self, model_output):
         softplus_activation = nn.Softplus()
-        output_size = output.shape[-1]
-        output_means = output[:, :, :output_size // 2]
-        output_vars = softplus_activation(output[:, :, output_size // 2:])
-        return output_means, output_vars
+        model_output_size = model_output.shape[-1]
+        model_output_means = model_output[:, :, :model_output_size // 2]
+        model_output_vars = softplus_activation(model_output[:, :, model_output_size // 2:])
+        return model_output_means, model_output_vars
