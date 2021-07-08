e2837687c40ece84e656ad74a311487a360960fe
pennfranc <flaessig@ethz.student.ch>
2021-07-08 19:21:03 +0200

    feat(tests): added TCNModel to probabilistic tests

--- darts/tests/test_probabilistic_models.py
+++ darts/tests/test_probabilistic_models.py
@@ -10,7 +10,7 @@ from darts.models.forecasting_model import GlobalForecastingModel
 logger = get_logger(__name__)
 
 try:
-    from ..models.rnn_model import RNNModel
+    from ..models import RNNModel, TCNModel
     from darts.utils.likelihood_models import GaussianLikelihoodModel
     TORCH_AVAILABLE = True
 except ImportError:
@@ -25,7 +25,9 @@ models_cls_kwargs_errs = [
 if TORCH_AVAILABLE:
     models_cls_kwargs_errs = np.concatenate([models_cls_kwargs_errs, [
         (RNNModel, {'input_chunk_length': 2, 'training_length': 10, 'n_epochs': 20, 'random_state': 0,
-                    'likelihood': GaussianLikelihoodModel()}, 1.9)
+                    'likelihood': GaussianLikelihoodModel()}, 1.9),
+        (TCNModel, {'input_chunk_length': 10, 'output_chunk_length': 5, 'n_epochs': 60, 'random_state': 0,
+                    'likelihood': GaussianLikelihoodModel()}, 0.28)
     ]])
 
 
