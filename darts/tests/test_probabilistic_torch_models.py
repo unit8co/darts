0677ed3820c0caa1bf99c6ebc3767f60908591a3
pennfranc <flaessig@ethz.student.ch>
2021-07-08 15:49:22 +0200

    feat(tests): finished tests for prob. torch models

--- darts/tests/test_probabilistic_torch_models.py
+++ darts/tests/test_probabilistic_torch_models.py
@@ -19,46 +19,67 @@ except ImportError:
 if TORCH_AVAILABLE:
 
     models_cls_kwargs_errs = [
-        (RNNModel, {'input_chunk_length': 2, 'training_length': 12, 'n_epochs': 10, 'random_state': 0,
-                    'likelihood': GaussianLikelihoodModel()}, 0.41)
+        (RNNModel, {'input_chunk_length': 2, 'training_length': 10, 'n_epochs': 10, 'random_state': 0,
+                    'likelihood': GaussianLikelihoodModel()}, 1.9)
     ]
 
     class ProbabilisticTorchModelsTestCase(DartsBaseTestClass):
 
-        sine_ts = tg.sine_timeseries(length=100)
-        sine_multivar_ts = sine_ts.stack(sine_ts)
+        np.random.seed(0)
+        linear_ts = tg.constant_timeseries(length=100)
+        linear_noisy_ts = linear_ts + tg.gaussian_timeseries(length=100, std=0.5)
+        linear_multivar_ts = linear_ts.stack(linear_ts)
+        linear_noisy_multivar_ts = linear_noisy_ts.stack(linear_noisy_ts)
         num_samples = 5
 
         def test_fit_predict_determinism(self):
+
             for model_cls, model_kwargs, _ in models_cls_kwargs_errs:
-                self.helper_test_fit_predict_determinism(model_cls, model_kwargs)
 
-        def helper_test_fit_predict_determinism(self, model_cls, model_kwargs):
+                # whether the first predictions of two models initiated with the same random state are the same
+                model = model_cls(**model_kwargs)
+                model.fit(self.linear_ts)
+                pred1 = model.predict(n=10, num_samples=2).values()
 
-            # whether the first predictions of two models initiated with the same random state are the same
-            model = model_cls(**model_kwargs)
-            model.fit(self.sine_ts)
-            pred1 = model.predict(n=10, num_samples=2).values()
+                model = model_cls(**model_kwargs)
+                model.fit(self.linear_ts)
+                pred2 = model.predict(n=10, num_samples=2).values()
 
-            model = model_cls(**model_kwargs)
-            model.fit(self.sine_ts)
-            pred2 = model.predict(n=10, num_samples=2).values()
+                self.assertTrue((pred1 == pred2).all())
 
-            self.assertTrue((pred1 == pred2).all())
-
-            # test whether the next prediction of the same model is different
-            pred3 = model.predict(n=10, num_samples=2).values()
-            self.assertTrue((pred2 != pred3).any())
+                # test whether the next prediction of the same model is different
+                pred3 = model.predict(n=10, num_samples=2).values()
+                self.assertTrue((pred2 != pred3).any())
 
         def test_probabilistic_forecast_accuracy(self):
             for model_cls, model_kwargs, err in models_cls_kwargs_errs:
-                self.help_test_probabilistic_forecast_accuracy(model_cls, model_kwargs, err, self.sine_ts)
-                self.help_test_probabilistic_forecast_accuracy(model_cls, model_kwargs, err, self.sine_multivar_ts)
+                self.helper_test_probabilistic_forecast_accuracy(model_cls, model_kwargs, err,
+                                                                 self.linear_ts, self.linear_noisy_ts)
+                self.helper_test_probabilistic_forecast_accuracy(model_cls, model_kwargs, err,
+                                                                 self.linear_multivar_ts,
+                                                                 self.linear_noisy_multivar_ts)
 
-        def help_test_probabilistic_forecast_accuracy(self, model_cls, model_kwargs, err, ts):
+        def helper_test_probabilistic_forecast_accuracy(self, model_cls, model_kwargs, err, ts, noisy_ts):
             model = model_cls(**model_kwargs)
-            model.fit(ts[:80], epochs=30)
-            pred = model.predict(n=20, num_samples=self.num_samples)
-            mape_err = mae(ts[20:], pred)
-            self.assertTrue(mape_err < err, 'Model {} produces errors too high (one time '
-                                            'series). Error = {}'.format(model_cls, mape_err))
+            model.fit(noisy_ts[:80], epochs=50)
+            pred = model.predict(n=20, num_samples=100)
+
+            # test median accuracy noiseless ts
+            mae_err_median = mae(ts[20:], pred)
+            self.assertLess(mae_err_median, err)
+
+            # test accuracy for increasing quantiles between 0.7 and 1 (it should decrease)
+            tested_quantiles = [0.7, 0.8, 0.9, 0.99]
+            mae_err = mae_err_median
+            for quantile in tested_quantiles:
+                new_mae = mae(ts[20:], pred.quantile_timeseries(quantile=quantile))
+                self.assertLess(mae_err, new_mae)
+                mae_err = new_mae
+
+            # test accuracy for decreasing quantiles between 0.3 and 0 (it should decrease)
+            tested_quantiles = [0.3, 0.2, 0.1, 0.01]
+            mae_err = mae_err_median
+            for quantile in tested_quantiles:
+                new_mae = mae(ts[20:], pred.quantile_timeseries(quantile=quantile))
+                self.assertLess(mae_err, new_mae)
+                mae_err = new_mae
