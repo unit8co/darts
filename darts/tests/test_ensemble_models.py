ace0235f5b635aa92a31301b296d258110d6f1e1
Michal Kosinski <michal.kosinski@unit8.co>
2021-07-22 14:07:11 +0200

    Update ensemble_model unit tests

--- darts/tests/test_ensemble_models.py
+++ darts/tests/test_ensemble_models.py
@@ -1,16 +1,29 @@
 import numpy as np
+import pandas as pd
 
 from .base_test_class import DartsBaseTestClass
+from .. import TimeSeries
 from ..utils import timeseries_generation as tg
 from ..models import NaiveDrift, NaiveSeasonal, Theta, ExponentialSmoothing
 from ..models import NaiveEnsembleModel
+from ..models import RNNModel, TCNModel, NBEATSModel
+
+
+def _make_ts(cls, n=0):
+    times = pd.date_range('20130101', '20130410')
+    pd_series = pd.Series(range(n, n + 100), index=times)
+    return TimeSeries.from_series(pd_series)
 
 
 class EnsembleModelsTestCase(DartsBaseTestClass):
     series1 = tg.sine_timeseries(value_frequency=(1 / 5), value_y_offset=10, length=50)
     series2 = tg.linear_timeseries(length=50)
 
-    def test_input_models(self):
+    seq1 = [_make_ts(0), _make_ts(10), _make_ts(20)]
+    cov1 = [_make_ts(5), _make_ts(15), _make_ts(25)]
+
+
+    def test_input_models_local_models(self):
         with self.assertRaises(ValueError):
             NaiveEnsembleModel([])
         with self.assertRaises(ValueError):
@@ -19,14 +32,42 @@ class EnsembleModelsTestCase(DartsBaseTestClass):
             NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal, Theta(), ExponentialSmoothing()])
         NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()])
 
-    def test_call_predict(self):
+    def test_call_predict_local_models(self):
         naive_ensemble = NaiveEnsembleModel([NaiveSeasonal(), Theta()])
         with self.assertRaises(Exception):
             naive_ensemble.predict(5)
         naive_ensemble.fit(self.series1)
         naive_ensemble.predict(5)
 
-    def test_predict_ensemble(self):
+    def test_predict_ensemble_local_models(self):
+        naive = NaiveSeasonal(K=5)
+        theta = Theta()
+        naive_ensemble = NaiveEnsembleModel([naive, theta])
+        naive_ensemble.fit(self.series1 + self.series2)
+        forecast_naive_ensemble = naive_ensemble.predict(5)
+        naive.fit(self.series1 + self.series2)
+        theta.fit(self.series1 + self.series2)
+        forecast_mean = 0.5 * naive.predict(5) + 0.5 * theta.predict(5)
+
+        self.assertTrue(np.array_equal(forecast_naive_ensemble.values(), forecast_mean.values()))
+
+    def test_input_models_global_models(self):
+        NaiveEnsembleModel([RNNModel(), TCNModel(10, 2), NBEATSModel(10, 2)])
+
+    def test_call_predict_global_models_univariate_input_no_covariates(self):
+        naive_ensemble = NaiveEnsembleModel([RNNModel(), TCNModel(10, 2), NBEATSModel(10, 2)])
+        with self.assertRaises(Exception):
+            naive_ensemble.predict(5)
+        naive_ensemble.fit(self.series1)
+        naive_ensemble.predict(5)
+
+    def test_call_predict_global_models_multivariate_input_no_covariates(self):
+        naive_ensemble = NaiveEnsembleModel([RNNModel(), TCNModel(10, 2), NBEATSModel(10, 2)])
+        naive_ensemble.fit(self.seq1)
+        naive_ensemble.predict(5)
+
+    # TODO
+    def test_predict_ensemble_global_models(self):
         naive = NaiveSeasonal(K=5)
         theta = Theta()
         naive_ensemble = NaiveEnsembleModel([naive, theta])
@@ -39,15 +80,18 @@ class EnsembleModelsTestCase(DartsBaseTestClass):
         self.assertTrue(np.array_equal(forecast_naive_ensemble.values(), forecast_mean.values()))
 
     def test_input_models_mixed(self):
-        pass
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([NaiveDrift(), Theta(), RNNModel()])
 
-    def test_fit_multivar_for_local_models(self):
-        # when local forecasting models are in ensemble, only instance of TS can be provided to fit()
-        pass
+    def test_fit_multivar_ts_with_local_models(self):
+        naive = NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()])
+        with self.assertRaises(ValueError):
+            naive.fit(self.seq1)
 
-    def test_fit_singlevar_with_covariates_for_local_models(self):
-        # when local forecasting models are in ensemble, no covariates should be provided
-        pass
+    def test_fit_univar_ts_with_covariates_for_local_models(self):
+        naive = NaiveEnsembleModel([NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()])
+        with self.assertRaises(ValueError):
+            naive.fit(self.series1, self.series2)
 
 
 if __name__ == '__main__':
