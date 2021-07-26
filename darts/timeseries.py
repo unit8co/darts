0c43411915cd12a9fef7e364389d644be82bcc4b
Maxime <dumjax@gmail.com>
2021-07-26 16:28:03 +0100

    clean docstring for with_columns_renamed

--- darts/timeseries.py
+++ darts/timeseries.py
@@ -1803,29 +1803,38 @@ class TimeSeries:
         plt.legend()
         plt.title(self._xa.name);
 
-    def with_columns_renamed(self, cols_name: Union[List[str], str], cols_name_new: Union[List[str], str]) -> 'TimeSeries':
+    def with_columns_renamed(self, col_names: Union[List[str], str], col_names_new: Union[List[str], str]) -> 'TimeSeries':
         """
-        Changes a ts column name and returns a new TimeSeries instance
+        Changes ts column names and returns a new TimeSeries instance.
+
+        Parameters
+        -------
+        col_names
+            String or list of strings corresponding the the column names to be changed.
+        col_names_new
+            String or list of strings corresponding to the new column names. Must be the same length as col_names.
+
+        Returns
         -------
         TimeSeries
-            A new TimeSeries instance
+            A new TimeSeries instance.
         """
 
-        if isinstance(cols_name, str):
-            cols_name = [cols_name]
-        if isinstance(cols_name_new, str):
-            cols_name_new = [cols_name_new]
+        if isinstance(col_names, str):
+            col_names = [col_names]
+        if isinstance(col_names_new, str):
+            col_names_new = [col_names_new]
 
-        raise_if_not(all([(x in self.columns.to_list()) for x in cols_name]), 
-                                                    "Some column names in cols_name don't exist in the time series", logger)
+        raise_if_not(all([(x in self.columns.to_list()) for x in col_names]), 
+                                                    "Some column names in col_names don't exist in the time series.", logger)
         
-        raise_if_not(len(cols_name) == len(cols_name_new), 'Length of col_name_new list should be'
-                                                    ' equal to length of col_name list', logger)
+        raise_if_not(len(col_names) == len(col_names_new), 'Length of col_names_new list should be'
+                                                    ' equal to the length of col_names list.', logger)
 
 
         cols = self.components
 
-        for (o, n) in zip(cols_name, cols_name_new):
+        for (o, n) in zip(col_names, col_names_new):
             cols = [n if (c==o) else c for c in cols]
 
         new_xa = xr.DataArray(
