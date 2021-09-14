772add53f6af907b0208df8c5f51b27fcbaed50a
dennisbader <dennis.bader@gmx.ch>
2021-09-14 10:31:24 +0200

    added risk test to darts/tests/test_probabilistic_models.py
    - It is very similar to test_probabilistic_forecast_accuracy(). I created a new test for cleanliness. One could also just append the risk test to the accuracy test to save a few seconds of computations.

--- darts/tests/test_probabilistic_models.py
+++ darts/tests/test_probabilistic_models.py
@@ -2,11 +2,10 @@ import numpy as np
 
 from .base_test_class import DartsBaseTestClass
 from ..utils import timeseries_generation as tg
-from ..metrics import mae
+from ..metrics import mae, rho_risk
 from ..logging import get_logger
 from darts.models import ExponentialSmoothing, ARIMA
 from darts.models.forecasting_model import GlobalForecastingModel
-
 logger = get_logger(__name__)
 
 try:
@@ -68,16 +67,25 @@ class ProbabilisticTorchModelsTestCase(DartsBaseTestClass):
                                                                  self.constant_multivar_ts,
                                                                  self.constant_noisy_multivar_ts)
 
+    def test_probabilistic_forecast_risk(self):
+        for model_cls, model_kwargs, err in models_cls_kwargs_errs:
+            self.helper_test_probabilistic_forecast_risk(model_cls, model_kwargs, err,
+                                                             self.constant_ts, self.constant_noisy_ts)
+            if issubclass(model_cls, GlobalForecastingModel):
+                self.helper_test_probabilistic_forecast_risk(model_cls, model_kwargs, err,
+                                                                 self.constant_multivar_ts,
+                                                                 self.constant_noisy_multivar_ts)
+
     def helper_test_probabilistic_forecast_accuracy(self, model_cls, model_kwargs, err, ts, noisy_ts):
         model = model_cls(**model_kwargs)
         model.fit(noisy_ts[:100])
         pred = model.predict(n=100, num_samples=100)
 
-        # test accuracy of the median prediction compared to the noiseless ts
+        # test accuracy of the median prediction and quantile (=0.5) rho-risk compared to the noiseless ts
         mae_err_median = mae(ts[100:], pred)
         self.assertLess(mae_err_median, err)
 
-        # test accuracy for increasing quantiles between 0.7 and 1 (it should decrease)
+        # test accuracy for increasing quantiles between 0.7 and 1 (it should decrease, mae should increase),
         tested_quantiles = [0.7, 0.8, 0.9, 0.99]
         mae_err = mae_err_median
         for quantile in tested_quantiles:
@@ -85,10 +93,35 @@ class ProbabilisticTorchModelsTestCase(DartsBaseTestClass):
             self.assertLess(mae_err, new_mae)
             mae_err = new_mae
 
-        # test accuracy for decreasing quantiles between 0.3 and 0 (it should decrease)
+        # test accuracy for decreasing quantiles between 0.3 and 0 (it should decrease, mae should increase),
         tested_quantiles = [0.3, 0.2, 0.1, 0.01]
         mae_err = mae_err_median
         for quantile in tested_quantiles:
             new_mae = mae(ts[100:], pred.quantile_timeseries(quantile=quantile))
             self.assertLess(mae_err, new_mae)
             mae_err = new_mae
+
+    def helper_test_probabilistic_forecast_risk(self, model_cls, model_kwargs, err, ts, noisy_ts):
+        model = model_cls(**model_kwargs)
+        model.fit(noisy_ts[:100])
+        pred = model.predict(n=100, num_samples=100)
+
+        # test rho-risk (at quantile=0.5) compared to the noiseless ts
+        rrisk_median = rho_risk(ts[100:], pred, rho=0.5)
+        self.assertLess(rrisk_median, err)
+
+        # test risk for increasing quantiles between 0.75 and 1 should increase
+        tested_quantiles = [0.8, 0.9, 0.95, 0.99]  # some test cases actually have minimum risk around 0.75
+        rrisk_err = rho_risk(ts[100:], pred, rho=0.75)
+        for quantile in tested_quantiles:
+            new_rrisk = rho_risk(ts[100:], pred, rho=quantile)
+            self.assertLess(rrisk_err, new_rrisk)
+            rrisk_err = new_rrisk
+
+        # test risk for decreasing quantiles between 0.25 and 0 should increase
+        tested_quantiles = [0.2, 0.1, 0.05, 0.01]  # some test cases actually have minimum risk around 0.25
+        rrisk_err = rho_risk(ts[100:], pred, rho=0.25)
+        for quantile in tested_quantiles:
+            new_rrisk = rho_risk(ts[100:], pred, rho=quantile)
+            self.assertLess(rrisk_err, new_rrisk)
+            rrisk_err = new_rrisk
