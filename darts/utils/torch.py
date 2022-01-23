75f7194499db3a07e20ec0824c50ce4781d2cefb
dennisbader <dennis.bader@gmx.ch>
2022-01-23 20:41:04 +0100

    removed unused pl random state wrapper function

--- darts/utils/torch.py
+++ darts/utils/torch.py
@@ -71,31 +71,3 @@ def random_method(decorated: Callable[..., T]) -> Callable[..., T]:
             return decorated(self, *args, **kwargs)
 
     return decorator
-
-
-# TODO: this is probably not needed
-def random_method_pl(decorated: Callable[..., T]) -> Callable[..., T]:
-    """ Decorator usable on any method within a class that will provide an isolated torch random context.
-
-    The decorator will store a `_random_instance` property on the object in order to persist successive calls to the RNG
-
-    Parameters
-    ----------
-    decorated
-        A method to be run in an isolated torch random context.
-
-    """
-    # check that @random_method has been applied to a method.
-    raise_if_not(_is_method(decorated), "@random_method can only be used on methods.", logger)
-
-    @wraps(decorated)
-    def decorator(self, *args, **kwargs) -> T:
-        if "random_state" in kwargs.keys():  # at model creation
-            self._random_instance = kwargs.get("random_state", None)
-        elif not hasattr(self, "_random_instance"):
-            self._random_instance = randint(0, high=MAX_NUMPY_SEED_VALUE)
-
-        with fork_rng():
-            seed_everything(self._random_instance, workers=True)
-            return decorated(self, *args, **kwargs)
-    return decorator
