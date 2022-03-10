5d68304914e3f32153a3600230d4195b786f4d61
gian <98726300+gnwhr@users.noreply.github.com>
2022-03-10 08:39:28 +0100

    Update darts/models/forecasting/linear_regression_model.py
    Co-authored-by: Julien Herzen <julien@unit8.co>

--- darts/models/forecasting/linear_regression_model.py
+++ darts/models/forecasting/linear_regression_model.py
@@ -219,7 +219,7 @@ class LinearRegressionModel(RegressionModel, _LikelihoodMixin):
                 prediction = super().predict(
                     n, series, past_covariates, future_covariates, **kwargs
                 )
-                model_outputs.append(prediction._xa.to_numpy())
+                model_outputs.append(prediction.all_values(copy=False))
             model_outputs = np.concatenate(model_outputs, axis=-1)
             samples = self._sample_quantiles(model_outputs, num_samples)
 
