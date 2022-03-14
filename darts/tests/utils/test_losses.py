070ecec4214d460d1838fd9a52d0f43ea4fe4c67
Julien Herzen <julien@unit8.co>
2022-03-14 11:31:11 +0100

    add unit tests for losses

---
+++ darts/tests/utils/test_losses.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from darts.dataprocessing.transformers import Scaler
+from darts.datasets import AirPassengersDataset
+from darts.metrics import smape
+from darts.models import NBEATSModel
+from darts.tests.base_test_class import DartsBaseTestClass
+from darts.utils.losses import MapeLoss, SmapeLoss
+
+
+class LossesTestCase(DartsBaseTestClass):
+    air = AirPassengersDataset().load().astype(np.float32)
+    scaler = Scaler()
+    air_s = scaler.fit_transform(air)
+    air_train, air_val = air_s[:-36], air_s[-36:]
+
+    def test_smape_loss(self):
+        model = NBEATSModel(
+            input_chunk_length=24,
+            output_chunk_length=12,
+            num_stacks=4,
+            num_blocks=1,
+            layer_widths=64,
+            loss_fn=SmapeLoss(),
+            random_state=42,
+        )
+
+        model.fit(self.air_train, epochs=150)
+
+        pred = model.predict(n=36)
+
+        print("error")
+        self.assertLess(smape(pred, self.air_s), 12.0)
+
+    def test_mape_loss(self):
+        model = NBEATSModel(
+            input_chunk_length=24,
+            output_chunk_length=12,
+            num_stacks=4,
+            num_blocks=1,
+            layer_widths=64,
+            loss_fn=MapeLoss(),
+            random_state=42,
+        )
+
+        model.fit(self.air_train, epochs=100)
+
+        pred = model.predict(n=36)
+
+        print("error")
+        self.assertLess(smape(pred, self.air_s), 11.0)
