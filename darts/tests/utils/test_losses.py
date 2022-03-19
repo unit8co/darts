d9af40ce0d16702f4038f946225cce95e0f47993
Julien Herzen <julien@unit8.co>
2022-03-19 15:19:34 +0100

    simplify loss tests

--- darts/tests/utils/test_losses.py
+++ darts/tests/utils/test_losses.py
@@ -1,49 +1,41 @@
-import numpy as np
+import torch
 
-from darts.dataprocessing.transformers import Scaler
-from darts.datasets import AirPassengersDataset
-from darts.metrics import smape
-from darts.models import NBEATSModel
 from darts.tests.base_test_class import DartsBaseTestClass
-from darts.utils.losses import MapeLoss, SmapeLoss
+from darts.utils.losses import MAELoss, MapeLoss, SmapeLoss
 
 
 class LossesTestCase(DartsBaseTestClass):
-    air = AirPassengersDataset().load().astype(np.float32)
-    scaler = Scaler()
-    air_s = scaler.fit_transform(air)
-    air_train, air_val = air_s[:-36], air_s[-36:]
+    x = torch.tensor([1.1, 2.2, 0.6345, -1.436])
+    y = torch.tensor([1.5, 0.5])
+
+    def helper_test_loss(self, exp_loss_val, exp_w_grad, loss_fn):
+        W = torch.tensor([[0.1, -0.2, 0.3, -0.4], [-0.8, 0.7, -0.6, 0.5]])
+        W.requires_grad = True
+        y_hat = W @ self.x
+        lval = loss_fn(y_hat, self.y)
+        lval.backward()
+
+        print(lval)
+        self.assertTrue(torch.allclose(lval, exp_loss_val, atol=1e-3))
+        self.assertTrue(torch.allclose(W.grad, exp_w_grad, atol=1e-3))
 
     def test_smape_loss(self):
-        model = NBEATSModel(
-            input_chunk_length=24,
-            output_chunk_length=12,
-            num_stacks=4,
-            num_blocks=1,
-            layer_widths=64,
-            loss_fn=SmapeLoss(),
-            random_state=42,
+        exp_val = torch.tensor(0.7753)
+        exp_grad = torch.tensor(
+            [[-0.2843, -0.5685, -0.1640, 0.3711], [-0.5859, -1.1718, -0.3380, 0.7649]]
         )
-
-        model.fit(self.air_train, epochs=150)
-
-        pred = model.predict(n=36)
-
-        self.assertLess(smape(pred, self.air_s), 16.5)
+        self.helper_test_loss(exp_val, exp_grad, SmapeLoss())
 
     def test_mape_loss(self):
-        model = NBEATSModel(
-            input_chunk_length=24,
-            output_chunk_length=12,
-            num_stacks=4,
-            num_blocks=1,
-            layer_widths=64,
-            loss_fn=MapeLoss(),
-            random_state=42,
+        exp_val = torch.tensor(1.2937)
+        exp_grad = torch.tensor(
+            [[-0.3667, -0.7333, -0.2115, 0.4787], [-1.1000, -2.2000, -0.6345, 1.4360]]
         )
+        self.helper_test_loss(exp_val, exp_grad, MapeLoss())
 
-        model.fit(self.air_train, epochs=100)
-
-        pred = model.predict(n=36)
-
-        self.assertLess(smape(pred, self.air_s), 13.5)
+    def test_mae_loss(self):
+        exp_val = torch.tensor(1.0020)
+        exp_grad = torch.tensor(
+            [[-0.5500, -1.1000, -0.3173, 0.7180], [-0.5500, -1.1000, -0.3173, 0.7180]]
+        )
+        self.helper_test_loss(exp_val, exp_grad, MAELoss())
