99182fd15f416b933030a018c3e909349e57a000
Julien Herzen <julien@unit8.co>
2022-04-12 16:29:31 +0200

    add sba and tsb flavours of Croston

--- darts/models/forecasting/croston.py
+++ darts/models/forecasting/croston.py
@@ -4,16 +4,20 @@ Croston method
 """
 
 import numpy as np
-from statsforecast.models import croston_classic, croston_optimized
+from statsforecast.models import croston_classic, croston_optimized, croston_sba
+from statsforecast.models import tsb as croston_tsb
 
+from darts.logging import raise_if, raise_if_not
 from darts.models.forecasting.forecasting_model import ForecastingModel
 from darts.timeseries import TimeSeries
 
 
 class Croston(ForecastingModel):
-    def __init__(self, optimized: bool = False):
+    def __init__(
+        self, version: str = "classic", alpha_d: float = None, alpha_p: float = None
+    ):
         """An implementation of the `Croston method
-        <https://otexts.com/fpp3/counts.html>`_. for intermittent
+        <https://otexts.com/fpp3/counts.html>`_ for intermittent
         count series.
 
         Relying on the implementation of `Statsforecasts package
@@ -21,13 +25,51 @@ class Croston(ForecastingModel):
 
         Parameters
         ----------
-        optimized
-            Whether to use the ``croston_optimized`` method, which searches
-            for the optimal ``alpha`` smoothing parameter and can take longer
-            to run. Otherwise, a fixed value of ``alpha=0.1`` is used.
+        version
+            - "classic" corresponds to classic Croston.
+            - "optimized" corresponds to optimized classic Croston, which searches
+              for the optimal ``alpha`` smoothing parameter and can take longer
+              to run. Otherwise, a fixed value of ``alpha=0.1`` is used.
+            - "sba" corresponds to the adjustment of the Croston method known as
+              the Syntetos-Boylan Approximation [1]_.
+            - "tsb" corresponds to the adjustment of the Croston method proposed by
+              Teunter, Syntetos and Babai [2]_. In this case, `alpha_d` and `alpha_p` must
+              be set.
+        alpha_d
+            For the "tsb" version, the alpha smoothing parameter to apply on demand.
+        alpha_p
+            For the "tsb" version, the alpha smoothing parameter to apply on probability.
+
+        References
+        ----------
+        .. [1] Aris A. Syntetos and John E. Boylan. The accuracy of intermittent demand estimates.
+               International Journal of Forecasting, 21(2):303 – 314, 2005.
+        .. [2] Ruud H. Teunter, Aris A. Syntetos, and M. Zied Babai.
+               Intermittent demand: Linking forecasting to inventory obsolescence.
+               European Journal of Operational Research, 214(3):606 – 615, 2011.
         """
         super().__init__()
-        self.method = croston_optimized if optimized else croston_classic
+        raise_if_not(
+            version.lower() in ["classic", "optimized", "sba", "tsb"],
+            'The provided "version" parameter must be set to "classic", "optimized", "sba" or "tsb".',
+        )
+
+        if version == "classic":
+            self.method = croston_classic
+        elif version == "optimized":
+            self.method = croston_optimized
+        elif version == "sba":
+            self.method = croston_sba
+        else:
+            raise_if(
+                alpha_d is None or alpha_p is None,
+                'alpha_d and alpha_p must be specified when using "tsb".',
+            )
+            self.method = croston_tsb
+            self.alpha_d = alpha_d
+            self.alpha_p = alpha_p
+
+        self.version = version
 
     def __str__(self):
         return "Croston"
@@ -36,9 +78,19 @@ class Croston(ForecastingModel):
         super().fit(series)
         series._assert_univariate()
         series = self.training_series
-        self.forecast_val = self.method(
-            series.values(copy=False), h=1, future_xreg=None
-        )
+
+        if self.version == "tsb":
+            self.forecast_val = self.method(
+                series.values(copy=False),
+                h=1,
+                future_xreg=None,
+                alpha_d=self.alpha_d,
+                alpha_p=self.apha_p,
+            )
+        else:
+            self.forecast_val = self.method(
+                series.values(copy=False), h=1, future_xreg=None
+            )
         return self
 
     def predict(
