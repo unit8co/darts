4cf16f7ffa9c2a396c47c572777e45454e2ac414
adamkells <adamjkells93@gmail.com>
2022-08-26 12:05:40 +0100

    first commit

--- darts/models/forecasting/regression_model.py
+++ darts/models/forecasting/regression_model.py
@@ -49,6 +49,7 @@ class RegressionModel(GlobalForecastingModel):
         lags_future_covariates: Union[Tuple[int, int], List[int]] = None,
         output_chunk_length: int = 1,
         add_encoders: Optional[dict] = None,
+        window_transformations: Optional[dict] = None,
         model=None,
     ):
         """Regression Model
@@ -105,6 +106,7 @@ class RegressionModel(GlobalForecastingModel):
         self.lags = {}
         self.output_chunk_length = None
         self.input_dim = None
+        self.window_transformations = window_transformations
 
         # model checks
         if self.model is None:
@@ -368,6 +370,73 @@ class RegressionModel(GlobalForecastingModel):
         y = np.concatenate(ys, axis=0)
         return X, y
 
+    def _create_windowed_data(
+        self, target_series, past_covariates, future_covariates, max_samples_per_ts
+    ):
+        if isinstance(target_series, TimeSeries):
+            target_series = [target_series]
+            past_covariates = [past_covariates] if past_covariates else None
+            future_covariates = [future_covariates] if future_covariates else None
+
+        Xs = []
+        # iterate over series
+        for idx, target_ts in enumerate(target_series):
+            covariates = [
+                (
+                    past_covariates[idx].pd_dataframe(copy=False)
+                    if past_covariates
+                    else None,
+                    self.window_transformations.get("past"),
+                ),
+                (
+                    future_covariates[idx].pd_dataframe(copy=False)
+                    if future_covariates
+                    else None,
+                    self.window_transformations.get("future"),
+                ),
+            ]
+
+            df_X = []
+            df_target = target_ts.pd_dataframe(copy=False)
+
+            # X: target lags
+            # TODO: Handle the different string to function options.
+            if "target" in self.window_transformations:
+                if self.window_transformations["target"] is dict:
+                    df_X.append(df_target)
+                # elif self.window_transformations["target"] is list:
+                #     for transformation in self.window_transformations["target"]:
+                #         df_X.append(df_target.shift(-lag))
+
+            # X: covariate lags
+            for df_cov, lags in covariates:
+                if lags:
+                    for lag in lags:
+                        df_X.append(df_cov.shift(-lag))
+
+            # combine lags
+            df_X = pd.concat(df_X, axis=1)
+            X = df_X.dropna().values
+
+            # keep most recent max_samples_per_ts samples
+            if max_samples_per_ts:
+                X = X[-max_samples_per_ts:]
+
+            raise_if(
+                X.shape[0] == 0,
+                "Unable to build any training samples of the target series "
+                + (f"at index {idx} " if len(target_series) > 1 else "")
+                + "and the corresponding covariate series; "
+                "There is no time step for which all required lags are available and are not NaN values.",
+            )
+
+            Xs.append(X)
+
+        # combine samples from all series
+        X = np.concatenate(Xs, axis=0)
+
+        return X
+
     def _fit_model(
         self,
         target_series,
@@ -384,6 +453,13 @@ class RegressionModel(GlobalForecastingModel):
         training_samples, training_labels = self._create_lagged_data(
             target_series, past_covariates, future_covariates, max_samples_per_ts
         )
+
+        # If window transformations have been specified then create the features and concat to the lagged data.
+        if self.window_transformations:
+            windowed_training_samples = self._create_windowed_data()
+            training_samples = pd.concat(
+                [training_samples, windowed_training_samples], axis=1
+            )
 
         # if training_labels is of shape (n_samples, 1) flatten it to shape (n_samples,)
         if len(training_labels.shape) == 2 and training_labels.shape[1] == 1:
