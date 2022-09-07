3cff4a66f4aab2a757d2a675b99e440f8d711810
Rijk van der Meulen <87600082+rijkvandermeulen@users.noreply.github.com>
2022-09-07 13:46:36 +0200

    Fix/#1185 corrected type hinting ExponentialSmoothing model (#1186)
    * #1185 Fixed type hinting ExponentialSmoothing model
    
    * #1185 Also change docstrings
    
    * seasonal instead of season_mode
    
    Co-authored-by: Julien Herzen <j.herzen@gmail.com>
    
    * #1185 fix docstrings
    
    Co-authored-by: Rijk van der Meulen <rijk.vandermeulen@eyeon.nl>
    Co-authored-by: Julien Herzen <j.herzen@gmail.com>
    Co-authored-by: Julien Herzen <julien@unit8.co>

--- darts/models/forecasting/exponential_smoothing.py
+++ darts/models/forecasting/exponential_smoothing.py
@@ -21,7 +21,7 @@ class ExponentialSmoothing(ForecastingModel):
         self,
         trend: Optional[ModelMode] = ModelMode.ADDITIVE,
         damped: Optional[bool] = False,
-        seasonal: Optional[ModelMode] = SeasonalityMode.ADDITIVE,
+        seasonal: Optional[SeasonalityMode] = SeasonalityMode.ADDITIVE,
         seasonal_periods: Optional[int] = None,
         random_state: int = 0,
         **fit_kwargs,
@@ -34,7 +34,10 @@ class ExponentialSmoothing(ForecastingModel):
         <https://www.statsmodels.org/stable/generated/statsmodels.tsa.holtwinters.ExponentialSmoothing.html>`_;
         we refer to this link for the original and more complete documentation of the parameters.
 
-        `model_mode` must be a ``ModelMode`` Enum member. You can access the Enum with ``from darts import ModelMode``.
+        `trend` must be a ``ModelMode`` Enum member. You can access the Enum with
+         ``from darts.utils.utils import ModelMode``.
+        `seasonal` must be a ``SeasonalityMode`` Enum member. You can access the Enum with
+        ``from darts.utils.utils import SeasonalityMode``.
 
         ``ExponentialSmoothing(trend=ModelMode.NONE, seasonal=SeasonalityMode.NONE)`` corresponds to a single
         exponential smoothing.
