b9d68cdfca3355e84aa5764177ce7d20f0cbc110
dennisbader <dennis.bader@gmx.ch>
2022-10-29 14:20:50 +0200

    update docs conf file with additional member exclusions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,9 +49,9 @@ autodoc_default_options = {
     "inherited-members": None,
     "show-inheritance": None,
     "exclude-members": "ForecastingModel,LocalForecastingModel,FutureCovariatesLocalForecastingModel,"
-    + "TorchForecastingModel,PastCovariatesTorchModel,FutureCovariatesTorchModel,DualCovariatesTorchModel,"
-    + "MixedCovariatesTorchModel,SplitCovariatesTorchModel,"
-    + "TorchParametricProbabilisticForecastingModel,"
+    + "TransferableFutureCovariatesLocalForecastingModel,GlobalForecastingModel,TorchForecastingModel,"
+    + "PastCovariatesTorchModel,FutureCovariatesTorchModel,DualCovariatesTorchModel,MixedCovariatesTorchModel,"
+    + "SplitCovariatesTorchModel,TorchParametricProbabilisticForecastingModel,"
     + "min_train_series_length,uses_future_covariates,uses_past_covariates,"
     + "untrained_model,first_prediction_index,future_covariate_series,past_covariate_series,"
     + "initialize_encoders,register_datapipe_as_function,register_function,functions,"
