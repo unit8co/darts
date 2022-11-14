7f5c52f1ccbbdd22261ff81841e73522b66e5b9c
Greg DeVos <gdevos010@gmail.com>
2022-11-14 13:11:36 -0800

    DLinear and NLinear Documentation (#1358)
    * documentation
    
    * set torch random seed
    
    * updated readme to show future models

--- darts/models/forecasting/dlinear.py
+++ darts/models/forecasting/dlinear.py
@@ -319,7 +319,7 @@ class DLinearModel(MixedCovariatesTorchModel):
             will be used as index encoders. Additionally, a transformer such as Darts' :class:`Scaler` can be added to
             transform the generated covariates. This happens all under one hood and only needs to be specified at
             model creation.
-            Read :meth:`SequentialEncoder <darts.utils.data.encoders.SequentialEncoder>` to find out more about
+            Read :meth:`SequentialEncoder <darts.dataprocessing.encoders.SequentialEncoder>` to find out more about
             ``add_encoders``. Default: ``None``. An example showing some of ``add_encoders`` features:
 
             .. highlight:: python
@@ -328,7 +328,7 @@ class DLinearModel(MixedCovariatesTorchModel):
                 add_encoders={
                     'cyclic': {'future': ['month']},
                     'datetime_attribute': {'future': ['hour', 'dayofweek']},
-                    'position': {'past': ['absolute'], 'future': ['relative']},
+                    'position': {'past': ['relative'], 'future': ['relative']},
                     'custom': {'past': [lambda idx: (idx.year - 1950) / 50]},
                     'transformer': Scaler()
                 }
