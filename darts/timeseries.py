ec78594e924afa10da6bab17e232a5ffc6e76465
Vladislav <79373600+tranquilitysmile@users.noreply.github.com>
2022-11-17 18:17:37 +0300

    Quantiles_df method speed optimization with wrap class of Quantile_timeseries (#1351)
    * Optimisation for quantiles_df using wrap of quantile_timeseries method
    
    * sync with actual repo
    
    * fix empty space
    
    * Fix mistake 
    
    Fix mistake with bool toggle
    
    * Modify quantiles_df method for using quantile_timeseries as main way to estimate result
    
    * Add unit test for quantiles_df method

--- darts/timeseries.py
+++ darts/timeseries.py
@@ -1517,7 +1517,7 @@ class TimeSeries:
         )
 
         # component names
-        cnames = [f"{comp}_quantiles" for comp in self.components]
+        cnames = [f"{comp}_{quantile}" for comp in self.components]
 
         new_data = np.quantile(
             self._xa.values,
@@ -1559,8 +1559,13 @@ class TimeSeries:
         pandas.DataFrame
             The Pandas DataFrame containing the quantiles for each component.
         """
-        # TODO: there might be a slightly more efficient way to do it for several quantiles at once with xarray...
-        return pd.concat([self.quantile_df(quantile) for quantile in quantiles], axis=1)
+        return pd.concat(
+            [
+                self.quantile_timeseries(quantile).pd_dataframe()
+                for quantile in quantiles
+            ],
+            axis=1,
+        )
 
     def astype(self, dtype: Union[str, np.dtype]) -> "TimeSeries":
         """
@@ -3033,7 +3038,6 @@ class TimeSeries:
             new_xa.values = fn(self._xa.values)
 
         elif num_args == 2:  # map function uses timestamp f(timestamp, x)
-
             # go over shortest amount of iterations, either over time steps or components and samples
             if self.n_timesteps <= self.n_components * self.n_samples:
                 new_vals = np.vstack(
