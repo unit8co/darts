370ed8aa26216e0667ca7207fc28ddafc1daa8c8
Blazej Nowicki <blazej.nowicki@unit8.co>
2022-11-21 13:53:21 +0100

    Update tests that use baseline models

--- darts/tests/models/forecasting/test_backtesting.py
+++ darts/tests/models/forecasting/test_backtesting.py
@@ -199,7 +199,7 @@ class BacktestingTestCase(DartsBaseTestClass):
 
         # univariate model + multivariate series
         with self.assertRaises(AssertionError):
-            NaiveDrift().backtest(
+            FFT().backtest(
                 linear_series_multi, start=pd.Timestamp("20000201"), forecast_horizon=3
             )
 
