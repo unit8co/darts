34ccc42281adf97176977431cec3f306e04f2a37
mabilton <Matt.A.Bilton@gmail.com>
2022-11-24 17:34:56 +1300

    Added documentation to `Diff` unit tests

--- darts/tests/dataprocessing/transformers/test_diff.py
+++ darts/tests/dataprocessing/transformers/test_diff.py
@@ -1,5 +1,6 @@
 import unittest
 from copy import deepcopy
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -20,7 +21,32 @@ class DiffTestCase(unittest.TestCase):
         axis=1,
     )
 
-    def assert_series_equal(self, series1, series2, equal_nan, to_compare=None):
+    def assert_series_equal(
+        self,
+        series1: TimeSeries,
+        series2: TimeSeries,
+        equal_nan: bool,
+        to_compare: Optional[np.ndarray] = None,
+    ):
+        """
+        Helper to compare series differenced by `Diff`.
+
+        Parameters
+        ----------
+            series1
+                First `TimeSeries` object to compare.
+            series2
+                Second `TimeSeries` object to compare.
+            equal_nan
+                Whether to compare `NaN` values as equal (see `help(np.testing.assert_allclose)`); should set
+                `equal_nan=~dropna` (i.e. should throw error if there are `NaN` values when user has requested
+                for them to be dropped).
+            to_compare
+                boolean `np.ndarray` which specifies which subset of columns should be compared in `series1` and
+                `series2`. Used when checking that components not specified by `component_mask` have NOT been
+                differenced after `fit`ing (i.e. `to_compare = ~component_mask`). If `to_compare = None`, all
+                columns in `series1` and `series2` are compared.
+        """
         if to_compare is not None:
             series1 = series1.drop_columns(series1.columns[~to_compare])
             series2 = series2.drop_columns(series2.columns[~to_compare])
@@ -30,8 +56,10 @@ class DiffTestCase(unittest.TestCase):
         self.assertTrue(series1.time_index.equals(series2.time_index))
 
     def test_diff_quad_series(self):
-        # Cumulative summing linear series = quadratic series; test that
-        # differencing quadratic series = linear series
+        """
+        Tests that differencing a quadratic series (computed by `cumsum`ing a linear series)
+        yields the original linear series.
+        """
         lin_series = linear_timeseries(start_value=1, end_value=10, length=50)
         quad_series = TimeSeries(lin_series.data_array().cumsum(axis=0))
         for dropna in (False, True):
@@ -52,9 +80,14 @@ class DiffTestCase(unittest.TestCase):
             )
 
     def test_diff_inverse_transform_beyond_fit_data(self):
-        # Test that Diff class can 'undifference' data that extends beyond
-        # what was used to fit it (e.g. like when undifferencing a forecast of a differenced timeseries)
+        """
+        Tests that `Diff` class can 'undifference' data that extends beyond
+         what was used to fit it (e.g. like when undifferencing a forecast of
+         a differenced timeseries, so as to a yield a forecast of the undifferenced
+         time series)
+        """
 
+        # (`lag`, `dropna`) pairs:
         test_cases = [
             (1, True),
             (1, False),
@@ -62,22 +95,31 @@ class DiffTestCase(unittest.TestCase):
             ([1, 2, 3, 2, 1], False),
         ]
 
+        # Artifically truncate series:
         short_sine = self.sine_series.copy().drop_after(10)
         for (lags, dropna) in test_cases:
-            # Fit Diff to subsection of data:
+            # Fit Diff to truncated series:
             diff = Diff(lags=lags, dropna=dropna)
             diff.fit(short_sine)
             # Difference entire time series:
             to_undiff = self.sine_series.copy()
             for lag in always_iterable(lags):
                 to_undiff = to_undiff.diff(n=1, periods=lag, dropna=dropna)
-            # Should be able to undifference entire series even though only fitted on start:
+            # Should be able to undifference entire series even though only fitted
+            # to truncated series:
             self.assert_series_equal(
                 self.sine_series, diff.inverse_transform(to_undiff), equal_nan=~dropna
             )
 
     def test_diff_multi_ts(self):
-        # Don't test dropna = True AND mask is not None, since this should throw error (see):
+        """
+        Tests that `Diff` correctly behaves when given multiple time series, both when a `component_mask`
+        is provided and when one is not.
+        """
+
+        # (`lags`, `dropna`, `component_mask`) triplets; note that combination of `dropna = True``
+        # AND `component_mask is not None` is not tested here, since this should throw error
+        # (see `test_diff_dropna_and_component_mask_specified`):
         component_mask = np.array([True, False, True])
         test_cases = [
             (1, True, None),
@@ -112,6 +154,10 @@ class DiffTestCase(unittest.TestCase):
             self.assert_series_equal(self.sine_series, back[1], equal_nan=~dropna)
 
     def test_diff_stochastic_series(self):
+        """
+        Tests that `Diff` class correctly differences and then undifferences a
+        random series with multiple samples.
+        """
         test_cases = [
             (1, True),
             (1, False),
@@ -126,9 +172,15 @@ class DiffTestCase(unittest.TestCase):
             transformer = Diff(lags=lags, dropna=dropna)
             new_series = transformer.fit_transform(series)
             series_back = transformer.inverse_transform(new_series)
+            # Should recover original series:
             self.assert_series_equal(series, series_back, equal_nan=~dropna)
 
     def test_diff_dropna_and_component_mask_specified(self):
+        """
+        Tests that `Diff` throws error during `fit` if `component_mask` is specified
+        when `dropna = True`; can't allow this since undifferenced components will be different
+        length to differenced components.
+        """
         diff = Diff(lags=1, dropna=True)
         with self.assertRaises(ValueError) as e:
             diff.fit(self.sine_series, component_mask=np.array([1, 0, 1], dtype=bool))
@@ -138,6 +190,10 @@ class DiffTestCase(unittest.TestCase):
         )
 
     def test_diff_series_too_short(self):
+        """
+        Tests that `Diff` throws error is length of series is less than `sum(lags)` (i.e.
+        there's not enough data to fit the differencer)
+        """
         lags = tuple(always_iterable(1000))
         diff = Diff(lags=lags)
         with self.assertRaises(ValueError) as e:
@@ -152,6 +208,10 @@ class DiffTestCase(unittest.TestCase):
         )
 
     def test_diff_incompatible_inverse_transform_date(self):
+        """
+        Tests that `Diff` throws error when given series to `inverse_transform`
+        which starts at a date not equal to the first data of the fitting data.
+        """
         vals = np.random.rand(10, 5)
         series1 = TimeSeries.from_times_and_values(
             values=vals, times=pd.date_range(start="1/1/2018", freq="d", periods=10)
@@ -179,6 +239,10 @@ class DiffTestCase(unittest.TestCase):
             )
 
     def test_diff_incompatible_inverse_transform_freq(self):
+        """
+        Tests that `Diff` throws error when given series to `inverse_transform`
+        that has different frequency than fitting data.
+        """
         vals = np.random.rand(10, 5)
         series1 = TimeSeries.from_times_and_values(
             values=vals, times=pd.date_range(start="1/1/2018", freq="W", periods=10)
@@ -196,6 +260,10 @@ class DiffTestCase(unittest.TestCase):
         )
 
     def test_diff_incompatible_inverse_transform_shape(self):
+        """
+        Tests that `Diff` throws error when given series to `inverse_transform`
+        that has different number of components or samples than the fitting data.
+        """
         vals = np.random.rand(10, 5, 5)
         dates = pd.date_range(start="1/1/2018", freq="W", periods=10)
         series = TimeSeries.from_times_and_values(values=vals, times=dates)
@@ -224,8 +292,7 @@ class DiffTestCase(unittest.TestCase):
 
     def test_diff_multiple_calls_to_fit(self):
         """
-        This test checks whether calling the scaler twice is calculating new lambdas instead of
-        keeping the old ones
+        Tests that `Diff` updates `start_vals` parameter when refitted to new data.
         """
         diff = Diff(lags=[2, 2], dropna=True)
 
