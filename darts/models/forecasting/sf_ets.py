151db2d93f1ee63b584059d79b5fca7f786ef679
Błażej Nowicki <78689854+BlazejNowicki@users.noreply.github.com>
2022-11-28 12:58:05 +0100

    Feat/multivariate on baseline models (#1373)
    * Modify the NaiveMean to accept multivariate ts
    
    * Allow multivariate ts in baseline models
    
    * Fix inheritance
    
    * Update tests that use baseline models
    
    * Fixed linting
    
    * Review suggestions
    
    * Add determinism check for local models
    
    * Add compatibility checks for uni/multivariate ts.
    
    Co-authored-by: Blazej Nowicki <blazej.nowicki@unit8.co>
    Co-authored-by: Julien Herzen <julien@unit8.co>

--- darts/models/forecasting/sf_ets.py
+++ darts/models/forecasting/sf_ets.py
@@ -58,7 +58,7 @@ class StatsForecastETS(FutureCovariatesLocalForecastingModel):
 
     def _fit(self, series: TimeSeries, future_covariates: Optional[TimeSeries] = None):
         super()._fit(series, future_covariates)
-        series._assert_univariate()
+        self._assert_univariate(series)
         series = self.training_series
         self.model.fit(
             series.values(copy=False).flatten(),
