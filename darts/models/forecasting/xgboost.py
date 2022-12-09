6df5c89411989d3e9916761a3c254e2277d13e4a
Julien Herzen <julien@unit8.co>
2022-12-09 15:53:22 +0100

    Update darts/models/forecasting/xgboost.py
    Co-authored-by: eliane-maalouf <112691612+eliane-maalouf@users.noreply.github.com>

--- darts/models/forecasting/xgboost.py
+++ darts/models/forecasting/xgboost.py
@@ -236,7 +236,7 @@ class XGBModel(RegressionModel, _LikelihoodMixin):
 
     @property
     def min_train_series_length(self) -> int:
-        # LightGBM requires a minimum of 2 train samples, therefore the min_train_series_length should be one more than
+        #  XGBModel  requires a minimum of 2 training samples, therefore the min_train_series_length should be one more than
         # for other regression models
         return max(
             3,
