3d724fc0af0a437905f129bece5d88402ffa4eef
eliane-maalouf <112691612+eliane-maalouf@users.noreply.github.com>
2022-12-13 15:44:16 +0100

    - updated experiments.py

--- darts/experiments/experiments.py
+++ darts/experiments/experiments.py
@@ -1,5 +1,5 @@
 """
-    This file contains the basic structure to define, run and backup and experiment
+    This file contains the basic structures to define, run and backup benchmarking experiments
 """
 
 import datetime
@@ -10,6 +10,15 @@ from abc import ABC, abstractmethod
 from glob import glob
 from typing import List, Optional, Sequence, Tuple, Union
 from tqdm import tqdm
+from darts.utils.utils import series2seq
+
+import optuna
+from optuna.integration import PyTorchLightningPruningCallback
+from optuna.visualization import (
+    plot_optimization_history,
+    plot_contour,
+    plot_param_importances,
+)
 
 from darts.logging import (
     get_logger,
@@ -18,10 +27,22 @@ from darts.logging import (
     raise_log,
 )
 
+from darts.models import (
+    StatsForecastAutoARIMA, StatsForecastETS, LinearRegressionModel, FourTheta, Theta, DLinearModel, NBEATSModel,
+    NHiTSModel, NLinearModel, TFTModel, TransformerModel, LightGBMModel, CatBoostModel, XGBModel, RegressionEnsembleModel
+)
+from darts.datasets import (
+    ETTh1Dataset, ETTh2Dataset, ETTm1Dataset, ETTm2Dataset, ElectricityDataset, TrafficDataset, WeatherDataset,
+    ILINetDataset, ExchangeRateDataset
+)
+
 logger = get_logger(__name__)
 
 DEFAULT_RADNOM_SEED = 42
 DEFAULT_EXP_ROOT = "./experiment_runs/"
+
+DATASETS = [ETTh1Dataset, ETTh2Dataset, ETTm1Dataset, ETTm2Dataset, ElectricityDataset, TrafficDataset, WeatherDataset,
+    ILINetDataset, ExchangeRateDataset]
 
 class BaseExperiment(ABC):
     """
@@ -33,52 +54,90 @@ class BaseExperiment(ABC):
         Root directory path where all experiments information are going to be stored.
     dataset
         The dataset to train on.
-    optimizer
-        The optimizer to use.
-    epochs
-        The number of epochs to train for.
-    split
-        The split of the dataset to use for training, validation and testing.
-    verbose
-        Whether to print progress during training.
     """
 
-    def __init__(self, experiment_root:Optional[str] = None, random_state: Optional[int] = None):
-        super.__init__()
+    def __init__(self, experiment_root:Optional[str] = None, random_state: Optional[int] = None,
+                 experiment_name: Optional[str] = None):
+        super().__init__()
 
         self.experiment_root = experiment_root if experiment_root is not None else DEFAULT_EXP_ROOT
+        self.experiment_dir = os.path.join(self.experiment_root, experiment_name)
         self.random_state = random_state if random_state is not None else DEFAULT_RADNOM_SEED
 
         #check if experiment_dir exists and create it if not
         if not os.path.exists(self.experiment_root):
             os.makedirs(self.experiment_root)
 
-        #store start time
-        self.start_time = datetime.datetime.now()
+        #store experiment start time
+        self.start_exp_time = datetime.datetime.now()
 
-    @abstractmethod
+        self.experiment_name = experiment_name
+
+        #make experiment_root and specific experiment directory directory
+        try:
+            os.mkdir(self.experiment_root)
+            print(f"Root directory {self.experiment_root} created")
+        except:
+            print(f"Root directory {self.experiment_root} already exists")
+
+        try:
+            os.mkdir(self.experiment_dir)
+            print(f"Experiment directory {self.experiment_dir} created")
+        except:
+            print(f"Experiment directory {self.experiment_dir} already exists")
+
     def run(self):
         """
         Run the experiment.
         """
         pass
 
-    @abstractmethod
+
     def backup(self):
         """
         Backup the experiment.
         """
         pass
 
-    @abstractmethod
-    def _get_data(self):
+    def _get_data(self, dataset_cls, load_multivariate = False, split = 0.8, horizon = None):
         """
         Get the data.
+        """
+        # read data
+        if "multivariate" in dataset_cls.__init__.__code__.co_varnames:
+            dataset = dataset_cls(multivariate=load_multivariate).load()
+        else:
+            dataset = dataset_cls().load()
+
+        dataset = series2seq(dataset)
+
+        # split data
+        if horizon is not None:
+            train = [s[:-horizon] for s in dataset]
+            val = [s[-horizon:] for s in dataset]
+            self.horizon = horizon
+        else:
+            all_splits = [list(s.split_after(split)) for s in dataset]
+            train = [split[0] for split in all_splits]
+            val = [split[1] for split in all_splits]
+
+            self.split = split
+
+        return train, val
+
+    def _preprocess_data(self):
+        """
+        Transform the data.
+        """
+        pass
+
+    def _explore_data(self):
+        """
+        Explore the data.
         """
         pass
 
     # load model
-    @abstractmethod
     def _load_model(self):
         """
         Load the model.
@@ -86,7 +145,6 @@ class BaseExperiment(ABC):
         pass
 
     # optimize hyper-parameters
-    @abstractmethod
     def _optimize_hyperparameters(self):
         """
         Optimize hyper-parameters.
@@ -94,7 +152,6 @@ class BaseExperiment(ABC):
         pass
 
     # train model
-    @abstractmethod
     def _train_model(self):
         """
         Train the model.
@@ -102,7 +159,6 @@ class BaseExperiment(ABC):
         pass
 
     # validate model
-    @abstractmethod
     def _validate_model(self):
         """
         Validate the model.
@@ -110,25 +166,37 @@ class BaseExperiment(ABC):
         pass
 
     # test model
-    @abstractmethod
     def _test_model(self):
         """
         Test the model.
         """
         pass
 
-class BaselinesExperiment(BaseExperiment):
+    def _backup_exp(self):
+        """
+        Backup the experiment.
+        """
+        pass
+
+class StatsExperiment(BaseExperiment):
     """
-    Experiment class to run the baseline models.
+    Experiment class to run the baseline statistical models.
     """
 
-    models = []
+    MODELS = [StatsForecastAutoARIMA, StatsForecastETS, FourTheta, Theta] # TODO: addoptimized theta, complex ES
+    NAME = "Stats_Experiment"
 
     def __init__(self, experiment_root:Optional[str] = None, random_state: Optional[int] = None,
-                 experiment_name: Optional[str] = None, dataset: Optional[str] = None,
-                 models: Optional[List[str]] = None, epochs: Optional[int] = None,
-                split: Optional[Tuple[float, float, float]] = None, verbose: Optional[bool] = None):
-        super.__init__(experiment_root, random_state)
+                 experiment_name: Optional[str] = None, datasets: Optional[str] = None,
+                 models: Optional[List[str]] = None, verbose: Optional[bool] = False):
+
+        experiment_name = experiment_name if experiment_name is not None else self.NAME
+
+        super().__init__(experiment_root, random_state, experiment_name)
+
+        self.datasets = datasets if datasets is not None else DATASETS
+        self.models = models if models is not None else self.MODELS
+        self.verbose = verbose
 
     def run(self):
         """
@@ -149,3 +217,43 @@ class BaselinesExperiment(BaseExperiment):
         Backup the experiment.
         """
         pass
+
+class MLExperiment(BaseExperiment):
+    """
+        Experiment class to run machine learning models.
+    """
+
+    MODELS = [LinearRegressionModel, LightGBMModel, CatBoostModel, XGBModel]
+    NAME = "ML_Experiments"
+
+    def __init__(self, experiment_root:Optional[str] = None, random_state: Optional[int] = None,
+                 experiment_name: Optional[str] = None, datasets: Optional[str] = None,
+                 models: Optional[List[str]] = None, verbose: Optional[bool] = False):
+
+        experiment_name = experiment_name if experiment_name is not None else self.NAME
+
+        super().__init__(experiment_root, random_state, experiment_name)
+
+        self.datasets = datasets if datasets is not None else DATASETS
+        self.models = models if models is not None else self.MODELS
+        self.verbose = verbose
+
+
+class DLExperiment(BaseExperiment):
+    """
+            Experiment class to run deep learning models.
+    """
+
+    MODELS = [DLinearModel, NLinearModel, NBEATSModel, TFTModel, TransformerModel, NHiTSModel]
+    NAME = "DL_Experiments"
+
+    def __init__(self, experiment_root: Optional[str] = None, random_state: Optional[int] = None,
+                 experiment_name: Optional[str] = None, datasets: Optional[str] = None,
+                 models: Optional[List[str]] = None, verbose: Optional[bool] = False):
+        experiment_name = experiment_name if experiment_name is not None else self.NAME
+
+        super().__init__(experiment_root, random_state, experiment_name)
+
+        self.datasets = datasets if datasets is not None else DATASETS
+        self.models = models if models is not None else self.MODELS
+        self.verbose = verbose
