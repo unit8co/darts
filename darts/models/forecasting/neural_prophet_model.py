b4f2f18aa39fa1d0411377628a2a59e9e8efc90d
Blazej Nowicki <blazej.nowicki@unit8.co>
2022-12-15 20:21:11 +0100

    Add past covariates

--- darts/models/forecasting/neural_prophet_model.py
+++ darts/models/forecasting/neural_prophet_model.py
@@ -3,18 +3,18 @@ Neural Prophet
 ------------
 """
 
-from typing import Optional, Sequence, Tuple, Union
+from typing import Optional, Sequence, Union
 
 import neuralprophet
 import pandas as pd
 from neuralprophet.utils import fcst_df_to_latest_forecast
 
 from darts.logging import raise_if_not
-from darts.models.forecasting.forecasting_model import GlobalForecastingModel
+from darts.models.forecasting.forecasting_model import ForecastingModel
 from darts.timeseries import TimeSeries, concatenate
 
 
-class NeuralProphet(GlobalForecastingModel):
+class NeuralProphet(ForecastingModel):
     def __init__(self, n_lags: int = 0, n_forecasts: int = 1, **kwargs):
         super().__init__()
 
@@ -28,113 +28,99 @@ class NeuralProphet(GlobalForecastingModel):
 
     def fit(
         self,
-        series: Union[TimeSeries, Sequence[TimeSeries]],
-        past_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
-        future_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
+        series: TimeSeries,
+        past_covariates: Optional[TimeSeries] = None,
+        future_covariates: Optional[TimeSeries] = None,
     ) -> "NeuralProphet":
-        super().fit(series, past_covariates, future_covariates)
+        super().fit(series)
 
-        # TODO accept list of univariate series or one multivariate ???
-        # TODO series have to have the same frequency
-
-        self.training_series = self._as_sequence(series)
-        self.train_past_cov = (
-            self._as_sequence(past_covariates) if past_covariates is not None else None
-        )
-        self.train_future_cov = (
-            self._as_sequence(future_covariates)
-            if future_covariates is not None
-            else None
+        raise_if_not(
+            series.has_datetime_index,
+            "NeuralProphet model is limited to TimeSeries index with DatetimeIndex",
         )
 
-        fit_df = self._convert_ts_to_df(
-            self.model, self.training_series, self.train_past_cov, self.train_future_cov
+        raise_if_not(
+            past_covariates is None or self.n_lags > 0,
+            "Past covariates are only supported when auto-regression is enabled (n_lags > 1)",
         )
 
-        # TODO check if all time series has common frequency string
-        self.model.fit(fit_df, freq=series[0].freq_str)
+        self.training_series = series
+        fit_df = self._convert_ts_to_df(series)
 
+        if past_covariates is not None:
+            fit_df = self._add_past_covariate(self.model, fit_df, past_covariates)
+
+        # TODO add future covariates to df
+
+        self.model.fit(fit_df, freq=series.freq_str, minimal=True)
+
+        self.fit_df = fit_df
         return self
 
     def predict(
         self,
         n: int,
-        series: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
-        past_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
-        future_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
+        future_covariates: Optional[TimeSeries] = None,
         num_samples: int = 1,
         verbose: bool = False,
     ) -> Union[TimeSeries, Sequence[TimeSeries]]:
-        super().predict(
-            n, series, past_covariates, future_covariates, num_samples, verbose
-        )
-
-        if series is None:
-            series_list = self.training_series
-        else:
-            series_list = self._as_sequence(series)
+        super().predict(n, num_samples)
 
         raise_if_not(
             self.n_lags == 0 or n <= self.n_forecasts,
             "Auto-regression has been configured. `n` must be smaller than or equal to"
             "`n_forecasts` parameter in the constructor.",
         )
-        # TODO consider time series indexed by ints
-        # TODO check if series was used during training
 
-        predictions = []
-        for series in series_list:
-            df = self._convert_ts_to_df([series])
-            future_df = self.model.make_future_dataframe(df=df, periods=n)
-            forecast_df = self.model.predict(future_df)
+        future_df = self.model.make_future_dataframe(df=self.fit_df, periods=n)
+        forecast_df = self.model.predict(future_df)
 
-            predictions.append(
-                self._convert_df_to_ts(
-                    forecast_df, series.end_time(), series.components
-                )
-            )
-        return self._from_sequence(predictions)
-
-    def _convert_ts_to_df(
-        self,
-        model: neuralprophet.NeuralProphet,
-        series_list: Sequence[TimeSeries],
-        past_cov: Optional[Sequence[TimeSeries]],
-        future_cov: Optional[Sequence[TimeSeries]],
-    ):
-        raise_if_not(
-            len(past_cov) == 0 or len(past_cov) == len(series_list),
-            "Number of past covariates has to be zero or equal to number of fit time series.",
+        return self._convert_df_to_ts(
+            forecast_df,
+            self.training_series.end_time(),
+            self.training_series.components,
         )
 
-        dfs = []
+    def _convert_ts_to_df(self, series: TimeSeries) -> pd.DataFrame:
+        """Convert TimeSeries to pandas DataFrame format required by Neural Prophet"""
+        dfs = []  # ID y
 
-        for i, series in enumerate(series_list):
-            for component in series.components:
-                new_df = (
-                    series[component].pd_dataframe(copy=False).reset_index(names=["ds"])
-                )
-                component_df = (
-                    new_df[["ds", component]]
-                    .copy(deep=True)
-                    .rename(columns={component: "y"})
-                )
-                component_df["ID"] = component
-
-            if past_cov is not None:
-                for component in past_cov[i].components:
-                    covaraite_df = (
-                        past_cov[i].pd_dataframe(copy=False).reset_index(names=["ds"])
-                    )
-                    covaraite_df = covaraite_df[["ds", component]].copy(deep=True)
-
-                    # TODO add checks if past covariate has full coverage
-                    component_df = component_df.merge(covaraite_df, how="left", on="ds")
-                    model.add_lagged_regressor(names=component)
-
+        for component in series.components:
+            component_df = (
+                series[component]
+                .pd_dataframe(copy=False)
+                .reset_index(names=["ds"])
+                .filter(items=["ds", component])
+                .rename(columns={component: "y"})
+            )
+            component_df["ID"] = component
             dfs.append(component_df)
 
-        return pd.concat(dfs)
+        return pd.concat(dfs).copy(deep=True)
+
+    def _add_past_covariate(
+        self,
+        model: neuralprophet.NeuralProphet,
+        df: pd.DataFrame,
+        past_covariates: TimeSeries,
+    ) -> pd.DataFrame:
+        """Convert past covariates from TimeSeries and add them to DataFrame"""
+
+        # TODO add checks if past covariate Time series has enough coverage and the same frequency
+
+        for component in past_covariates.components:
+            covariate_df = (
+                past_covariates[component]
+                .pd_dataframe(copy=False)
+                .reset_index(names=["ds"])
+                .filter(items=["ds", component])
+            )
+
+            df = df.merge(covariate_df, how="left", on="ds")
+
+            model.add_lagged_regressor(names=component)
+
+        return df
 
     def _convert_df_to_ts(self, forecast: pd.DataFrame, last_train_date, components):
         groups = []
@@ -165,27 +151,6 @@ class NeuralProphet(GlobalForecastingModel):
             [TimeSeries.from_dataframe(group, time_col="ds") for group in groups],
             axis=1,
         )
-
-    def _as_sequence(
-        self, series: Optional[Union[TimeSeries, Sequence[TimeSeries]]]
-    ) -> Sequence[TimeSeries]:
-        if series is None:
-            return []
-
-        if isinstance(series, TimeSeries):
-            return [series]
-
-        return series
-
-    def _from_sequence(
-        self, series_list: Sequence[TimeSeries]
-    ) -> Union[TimeSeries, Sequence[TimeSeries]]:
-        if len(series_list) == 1:
-            return series_list[0]
-        return series_list
-
-    def _model_encoder_settings(self) -> Tuple[int, int, bool, bool]:
-        raise NotImplementedError()
 
     def __str__(self):
         return "Neural Prophet"
