bcf9abf6686bf8872026064cbe883beb6ac8e0ef
eliane-maalouf <112691612+eliane-maalouf@users.noreply.github.com>
2022-12-16 16:37:44 +0100

    refactors MovingAverage filter  (#1408)
    * - makes MovingAverage filter use window_transform() for TimeSeries
    - adds static covariates and hirarchy to the transformed series (output of window_transform())
    
    * - added the possibility to exclude the current timestep from the window computation (accounts for whether a "closed"="left" parameter is provided for "rolling" to avoid shifting twice)
    
    * - adapted WindowTransformer to account include_current
    - added unittest for include_current
    - corrected behavior with NaN in TimesSeries.window_transform()
    
    * - corrected formatting

--- darts/timeseries.py
+++ darts/timeseries.py
@@ -3162,6 +3162,7 @@ class TimeSeries:
         treat_na: Optional[Union[str, Union[int, float]]] = None,
         forecasting_safe: Optional[bool] = True,
         keep_non_transformed: Optional[bool] = False,
+        include_current: Optional[bool] = True,
     ) -> "TimeSeries":
         """
         Applies a moving/rolling, expanding or exponentially weighted window transformation over this ``TimeSeries``.
@@ -3253,6 +3254,9 @@ class TimeSeries:
             ``False`` to return the transformed components only, ``True`` to return all original components along
             the transformed ones. Default is ``False``.
 
+        include_current
+            ``True`` to include the current time step in the window, ``False`` to exclude it. Default is ``True``.
+
         Returns
         -------
         TimeSeries
@@ -3296,6 +3300,7 @@ class TimeSeries:
 
             # take expanding as the default window operation if not specified, safer than rolling
             mode = transformation.get("mode", "expanding")
+
             raise_if_not(
                 mode in PD_WINDOW_OPERATIONS.keys(),
                 f"Invalid window operation: '{mode}'. Must be one of {PD_WINDOW_OPERATIONS.keys()}.",
@@ -3443,6 +3448,15 @@ class TimeSeries:
                 transformation, forecasting_safe
             )
 
+            closed = transformation.get("closed", None)
+            if not include_current:
+                if window_mode == "rolling":
+                    shifts = 0 if closed == "left" else 1  # avoid shifting twice
+                else:
+                    shifts = 1
+            else:
+                shifts = 0
+
             resulting_transformations = pd.concat(
                 [
                     resulting_transformations,
@@ -3451,7 +3465,7 @@ class TimeSeries:
                             **window_mode_kwargs
                         ),
                         fn,
-                    )(**function_kwargs),
+                    )(**function_kwargs).shift(periods=shifts),
                 ],
                 axis=1,
             )
@@ -3470,9 +3484,10 @@ class TimeSeries:
             # track how many NaN rows are added by each transformation on each transformed column
             # NaNs would appear only if user changes "min_periods" to else than 1, if not,
             # by default there should be no NaNs unless the original series starts with NaNs (those would be maintained)
+            total_na = min_periods + shifts + (closed == "left")
             added_na.extend(
                 [
-                    min_periods - 1 if min_periods > 0 else min_periods
+                    total_na - 1 if min_periods > 0 else total_na
                     for _ in filter_df_columns
                 ]
             )
@@ -3525,6 +3540,8 @@ class TimeSeries:
                 len(new_index), -1, n_samples
             ),
             columns=new_columns,
+            static_covariates=self.static_covariates,
+            hierarchy=self.hierarchy,
         )
 
         return transformed_time_series
