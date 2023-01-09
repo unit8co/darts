4131cb090e30f66add0170620281627f7b7c978b
Beerstabr <boydbiersteker@gmail.com>
2023-01-09 14:17:46 +0100

    Added AutoTheta from the StatsForecast package.

--- darts/models/forecasting/sf_models.py
+++ darts/models/forecasting/sf_models.py
@@ -13,11 +13,13 @@ from typing import Optional
 import numpy as np
 from statsforecast.models import ETS
 from statsforecast.models import AutoARIMA as SFAutoARIMA
+from statsforecast.models import AutoTheta as SFAutoTheta
 
 from darts import TimeSeries
 from darts.models import LinearRegressionModel
 from darts.models.forecasting.forecasting_model import (
     FutureCovariatesLocalForecastingModel,
+    LocalForecastingModel,
 )
 
 
@@ -90,6 +92,7 @@ class StatsForecastETS(FutureCovariatesLocalForecastingModel):
         series = self.training_series
 
         if future_covariates is not None:
+            # perform OLS and get in-sample residuals
             linreg = LinearRegressionModel(lags_future_covariates=[0])
             linreg.fit(series, future_covariates=future_covariates)
             fitted_values = linreg.model.predict(
@@ -122,7 +125,8 @@ class StatsForecastETS(FutureCovariatesLocalForecastingModel):
             level=(68.27,),  # ask one std for the confidence interval
         )
 
-        mu_ets = forecast_dict["mean"]
+        mu_ets, std = unpack_sf_dict(forecast_dict)
+
         if future_covariates is not None:
             mu_linreg = self._linreg.predict(n, future_covariates=future_covariates)
             mu_linreg_values = mu_linreg.values().reshape(
@@ -133,7 +137,7 @@ class StatsForecastETS(FutureCovariatesLocalForecastingModel):
             mu = mu_ets
 
         if num_samples > 1:
-            samples = sf_create_samples(mu, forecast_dict, num_samples, n)
+            samples = create_normal_samples(mu, std, num_samples, n)
         else:
             samples = mu
         return self._build_forecast_series(samples)
@@ -232,9 +236,9 @@ class StatsForecastAutoARIMA(FutureCovariatesLocalForecastingModel):
             level=(68.27,),  # ask one std for the confidence interval.
         )
 
-        mu = forecast_dict["mean"]
+        mu, std = unpack_sf_dict(forecast_dict)
         if num_samples > 1:
-            samples = sf_create_samples(mu, forecast_dict, num_samples, n)
+            samples = create_normal_samples(mu, std, num_samples, n)
         else:
             samples = mu
 
@@ -251,14 +255,103 @@ class StatsForecastAutoARIMA(FutureCovariatesLocalForecastingModel):
         return True
 
 
-def sf_create_samples(
+class StatsForecastAutoTheta(LocalForecastingModel):
+    def __init__(
+        self, *autotheta_args, add_encoders: Optional[dict] = None, **autotheta_kwargs
+    ):
+        """Auto-Theta based on `Statsforecasts package
+        <https://github.com/Nixtla/statsforecast>`_.
+
+        Automatically selects the best Theta (Standard Theta Model (‘STM’), Optimized Theta Model (‘OTM’),
+        Dynamic Standard Theta Model (‘DSTM’), Dynamic Optimized Theta Model (‘DOTM’)) model using mse.
+        <https://www.sciencedirect.com/science/article/pii/S0169207016300243>
+
+        It is probabilistic, whereas :class:`FourTheta` is not.
+
+        We refer to the `statsforecast AutoTheta documentation
+        <https://nixtla.github.io/statsforecast/models.html#autotheta>`_
+        for the documentation of the arguments.
+
+        Parameters
+        ----------
+        autotheta_args
+            Positional arguments for ``statsforecasts.models.AutoTheta``.
+        autotheta_kwargs
+            Keyword arguments for ``statsforecasts.models.AutoTheta``.
+
+            ..
+
+        Examples
+        --------
+        >>> from darts.models import StatsForecastAutoTheta
+        >>> from darts.datasets import AirPassengersDataset
+        >>> series = AirPassengersDataset().load()
+        >>> model = StatsForecastAutoTheta(season_length=12)
+        >>> model.fit(series[:-36])
+        >>> pred = model.predict(36, num_samples=100)
+        """
+        super().__init__()
+        self.model = SFAutoTheta(*autotheta_args, **autotheta_kwargs)
+
+    def __str__(self):
+        return "Auto-Theta-Statsforecasts"
+
+    def fit(self, series: TimeSeries):
+        super().fit(series)
+        self._assert_univariate(series)
+        series = self.training_series
+        self.model.fit(
+            series.values(copy=False).flatten(),
+        )
+        return self
+
+    def predict(
+        self,
+        n: int,
+        num_samples: int = 1,
+        verbose: bool = False,
+    ):
+        super().predict(n, num_samples)
+        forecast_dict = self.model.predict(
+            h=n,
+            level=(68.27,),  # ask one std for the confidence interval.
+        )
+
+        mu, std = unpack_sf_dict(forecast_dict)
+        if num_samples > 1:
+            samples = create_normal_samples(mu, std, num_samples, n)
+        else:
+            samples = mu
+
+        return self._build_forecast_series(samples)
+
+    @property
+    def min_train_series_length(self) -> int:
+        return 10
+
+    def _supports_range_index(self) -> bool:
+        return True
+
+    def _is_probabilistic(self) -> bool:
+        return True
+
+
+def create_normal_samples(
     mu: float,
-    forecast_dict: dict,
+    std: float,
     num_samples: int,
     n: int,
-):
-    """Generate samples for StatsForecast Models"""
-    std = forecast_dict["hi-68.27"] - mu
+) -> np.array:
+    """Generate samples assuming a Normal distribution."""
     samples = np.random.normal(loc=mu, scale=std, size=(num_samples, n)).T
     samples = np.expand_dims(samples, axis=1)
     return samples
+
+
+def unpack_sf_dict(
+    forecast_dict: dict,
+):
+    """Unpack the dictionary that is returned by the StatsForecast 'predict()' method."""
+    mu = forecast_dict["mean"]
+    std = forecast_dict["hi-68.27"] - mu
+    return mu, std
