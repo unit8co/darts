2766c6854eead5ce00a4612f1964b1b8cdede57f
Matt Bilton <Matt.A.Bilton@gmail.com>
2023-01-15 13:02:22 +1300

    Clarification about `check_inputs` in docstring.

--- darts/utils/data/tabularization.py
+++ darts/utils/data/tabularization.py
@@ -13,163 +13,6 @@ from darts.timeseries import TimeSeries
 logger = get_logger(__name__)
 
 
-def _create_lagged_data(
-    target_series: Union[TimeSeries, Sequence[TimeSeries]],
-    output_chunk_length: int,
-    past_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
-    future_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
-    lags: Optional[Sequence[int]] = None,
-    lags_past_covariates: Optional[Sequence[int]] = None,
-    lags_future_covariates: Optional[Sequence[int]] = None,
-    max_samples_per_ts: Optional[int] = None,
-    is_training: Optional[bool] = True,  # other option: 'inference
-    multi_models: Optional[bool] = True,
-):
-    """
-    Helper function that creates training/validation matrices (X and y as required in sklearn), given series and
-    max_samples_per_ts.
-    X has the following structure:
-    lags_target | lags_past_covariates | lags_future_covariates
-    Where each lags_X has the following structure (lags_X=[-2,-1] and X has 2 components):
-    lag_-2_comp_1_X | lag_-2_comp_2_X | lag_-1_comp_1_X | lag_-1_comp_2_X
-    y has the following structure (output_chunk_length=4 and target has 2 components):
-    lag_+0_comp_1_target | lag_+0_comp_2_target | ... | lag_+3_comp_1_target | lag_+3_comp_2_target
-    Parameters
-    ----------
-    target_series
-        The target series of the regression model.
-    output_chunk_length
-        The output_chunk_length of the regression model.
-    past_covariates
-        Optionally, the past covariates of the regression model.
-    future_covariates
-        Optionally, the future covariates of the regression model.
-    lags
-        Optionally, the lags of the target series to be used as features.
-    lags_past_covariates
-        Optionally, the lags of the past covariates to be used as features.
-    lags_future_covariates
-        Optionally, the lags of the future covariates to be used as features.
-    max_samples_per_ts
-        Optionally, the maximum number of samples to be drawn for training/validation
-        The kept samples are the most recent ones.
-    is_training
-        Optionally, whether the data is used for training or inference.
-        If inference, the rows where the future_target_lags are NaN are not removed from X,
-        as we are only interested in the X matrix to infer the future target values.
-    """
-
-    # ensure list of TimeSeries format
-    if isinstance(target_series, TimeSeries):
-        target_series = [target_series]
-        past_covariates = [past_covariates] if past_covariates else None
-        future_covariates = [future_covariates] if future_covariates else None
-
-    Xs, ys, Ts = [], [], []
-
-    # iterate over series
-    for idx, target_ts in enumerate(target_series):
-        covariates = [
-            (
-                past_covariates[idx].pd_dataframe(copy=False)
-                if past_covariates
-                else None,
-                lags_past_covariates,
-            ),
-            (
-                future_covariates[idx].pd_dataframe(copy=False)
-                if future_covariates
-                else None,
-                lags_future_covariates,
-            ),
-        ]
-
-        df_X = []
-        df_y = []
-        df_target = target_ts.pd_dataframe(copy=False)
-
-        # y: output chunk length lags of target
-        if multi_models:
-            for future_target_lag in range(output_chunk_length):
-                df_y.append(
-                    df_target.shift(-future_target_lag).rename(
-                        columns=lambda x: f"{x}_horizon_lag{future_target_lag}"
-                    )
-                )
-        else:
-            df_y.append(
-                df_target.shift(-output_chunk_length + 1).rename(
-                    columns=lambda x: f"{x}_horizon_lag{output_chunk_length-1}"
-                )
-            )
-
-        if lags:
-            for lag in lags:
-                df_X.append(
-                    df_target.shift(-lag).rename(
-                        columns=lambda x: f"{x}_target_lag{lag}"
-                    )
-                )
-
-        # X: covariate lags
-        for covariate_name, (df_cov, lags_cov) in zip(["past", "future"], covariates):
-            if lags_cov:
-                if not is_training:
-                    # We extend the covariates dataframe
-                    # so that when we create the lags with shifts
-                    # we don't have nan on the last (or first) rows. Only useful for inference.
-                    df_cov = df_cov.reindex(df_target.index.union(df_cov.index))
-
-                for lag in lags_cov:
-                    df_X.append(
-                        df_cov.shift(-lag).rename(
-                            columns=lambda x: f"{x}_{covariate_name}_cov_lag{lag}"
-                        )
-                    )
-
-        # combine lags
-        df_X = pd.concat(df_X, axis=1)
-        df_y = pd.concat(df_y, axis=1)
-        df_X_y = pd.concat([df_X, df_y], axis=1)
-        if is_training:
-            df_X_y = df_X_y.dropna()
-        # We don't need to drop where y are none for inference, as we just care for X
-        else:
-            df_X_y = df_X_y.dropna(subset=df_X.columns)
-
-        Ts.append(df_X_y.index)
-        X_y = df_X_y.values
-
-        # keep most recent max_samples_per_ts samples
-        if max_samples_per_ts:
-            X_y = X_y[-max_samples_per_ts:]
-            Ts[-1] = Ts[-1][-max_samples_per_ts:]
-
-        raise_if(
-            X_y.shape[0] == 0,
-            "Unable to build any training samples of the target series "
-            + (f"at index {idx} " if len(target_series) > 1 else "")
-            + "and the corresponding covariate series; "
-            "There is no time step for which all required lags are available and are not NaN values.",
-        )
-
-        X, y = np.split(X_y, [df_X.shape[1]], axis=1)
-
-        Xs.append(X)
-        ys.append(y)
-
-    # combine samples from all series
-    X = np.concatenate(Xs, axis=0)
-    y = np.concatenate(ys, axis=0)
-
-    return X, y, Ts
-
-
-#
-#   Refactored Implementation:
-#
-
-
 def create_lagged_data(
     target_series: Optional[TimeSeries] = None,
     past_covariates: Optional[TimeSeries] = None,
@@ -291,8 +134,9 @@ def create_lagged_data(
         If `False`, then the regression model is assumed to predict *only* the timestep at `t+output_chunk_length`.
         This input is ignored if `is_training = False`.
     check_inputs
-        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Caution should be
-        exercised when setting `check_inputs = False`.
+        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Should be set
+        to `False` if inputs have already been checked for validity (e.g. inside the `__init__` of a class), otherwise
+        should be set to `True`.
     use_moving_windows
         Optionally, specifies that the 'moving window' method should be used to construct `X` and `y` if all of the
         provided series are of the same frequency. If `use_moving_windows = False`, the 'time intersection' method
@@ -432,8 +276,9 @@ def create_lagged_training_data(
         then the regression model is assumed to predict all of the timesteps from time `t` to `t+output_chunk_length`.
         If `False`, then the regression model is assumed to predict *only* the timestep at `t+output_chunk_length`.
     check_inputs
-        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Caution should be
-        exercised when setting `check_inputs = False`.
+        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Should be set
+        to `False` if inputs have already been checked for validity (e.g. inside the `__init__` of a class), otherwise
+        should be set to `True`.
     use_moving_windows
         Optionally, specifies that the 'moving window' method should be used to construct `X` and `y` if all of the
         provided series are of the same frequency. If `use_moving_windows = False`, the 'time intersection' method
@@ -524,8 +369,9 @@ def create_lagged_prediction_data(
         samples are kept. In theory, specifying a smaller `max_samples_per_ts` should reduce computation time,
         especially in cases where many observations could be generated.
     check_inputs
-        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Caution should be
-        exercised when setting `check_inputs = False`.
+        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Should be set
+        to `False` if inputs have already been checked for validity (e.g. inside the `__init__` of a class), otherwise
+        should be set to `True`.
     use_moving_windows
         Optionally, specifies that the 'moving window' method should be used to construct `X` and `y` if all of the
         provided series are of the same frequency. If `use_moving_windows = False`, the 'time intersection' method
@@ -911,8 +757,9 @@ def get_feature_times(
         Optionally, specifies that training data is to be generated from the specified series. If `True`,
         `target_series`, `output_chunk_length`, and `multi_models` must all be specified.
     check_inputs
-        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Caution should be
-        exercised when setting `check_inputs = False`.
+        Optionally, specifies that the `lags_*` and `series_*` inputs should be checked for validity. Should be set
+        to `False` if inputs have already been checked for validity (e.g. inside the `__init__` of a class), otherwise
+        should be set to `True`.
     return_min_and_max_lags
         Optionally, specifies whether the largest magnitude lag value for each series should also be returned along with
         the 'eligible' feature times
@@ -1188,8 +1035,9 @@ def strided_moving_window(
     axis
         Optionally, the axis along which the moving windows should be extracted.
     check_inputs
-        Optionally, specifies whether inputs should be checked for validity. Caution should be exercised when
-        setting to `False`; see [1]_ for further details.
+        Optionally, specifies whether inputs should be checked for validity. Should be set
+        to `False` if inputs have already been checked for validity (e.g. inside the `__init__`
+        of a class), otherwise should be set to `True`. See [1]_ for further details.
 
     Returns
     -------
