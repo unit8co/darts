e55bc8a48f697c240f9f510a89c7ef11acd7ee91
Beerstabr <boydbiersteker@gmail.com>
2023-02-27 21:23:54 +0100

    MIDASTransformer now outputs a low sample variant of the high sample series through 'ts_transform()'

---
+++ darts/dataprocessing/transformers/midas.py
@@ -0,0 +1,107 @@
+"""
+Mixed-data sampling (MIDAS) Transformer
+------------------
+"""
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from pandas import DateOffset, Timedelta
+
+from darts import TimeSeries
+from darts.dataprocessing.transformers import BaseDataTransformer
+from darts.logging import get_logger, raise_log
+
+logger = get_logger(__name__)
+
+
+def _assert_high_to_low_freq(high_freq_series_df: pd.DataFrame,
+                             low_freq_series_df: pd.DataFrame,
+                             rule,
+                             high_freq,
+                             ):
+    if not low_freq_series_df.shape[0] < high_freq_series_df.shape[0]:
+        raise_log(
+            ValueError(f"The target conversion should go from a high to a " \
+                       f"low frequency, instead the targeted frequency is" \
+                       f"{rule}, while the original frequency is {high_freq}.")
+        )
+
+
+class MIDASTransformer(BaseDataTransformer):
+    def __init__(
+            self,
+            rule: str,
+            strip: bool = True,
+            name: str = "MIDASTransformer",
+            n_jobs: int = 1,
+            verbose: bool = False,
+    ):
+        """
+        A transformer that converts higher frequency time series to lower frequency using mixed-data sampling.
+        """
+        super().__init__(name, n_jobs, verbose)
+        self.rule = rule
+        self.strip = strip
+
+    @staticmethod
+    def ts_transform(series: TimeSeries,
+                     rule: Union[DateOffset, Timedelta, str],
+                     strip: bool,
+                     ) -> TimeSeries:
+        high_freq = series.freq_str
+        series_df = series.pd_dataframe()
+        series_copy_df = series_df.copy()
+        series_df.index = series_df.index.to_period()
+        high_freq_period = series_df.index.freqstr
+
+        # first ensure the length of the series is an exact multiple of the length of the targeted low frequency series
+        # we do this by resampling from high to low and then back to high again
+        low_freq_series_df = series_df.resample(rule).last()
+        low_index = low_freq_series_df.index.to_timestamp()
+        high_freq_series_df = low_freq_series_df.resample(high_freq_period).bfill().ffill()
+        high_index = high_freq_series_df.index.to_timestamp()
+
+        _assert_high_to_low_freq(high_freq_series_df=high_freq_series_df,
+                                 low_freq_series_df=low_freq_series_df,
+                                 rule=rule,
+                                 high_freq=high_freq,
+                                 )
+
+        # if necessary, expand the original series
+        if len(high_index) > series_df.shape[0]:
+            series_df = pd.DataFrame(np.nan, index=high_index, columns=series_df.columns)
+            series_df.loc[series_copy_df.index, :] = series_copy_df.values
+        else:
+            series_df = series_copy_df
+
+
+        n_high = series_df.shape[0]
+        n_low = low_freq_series_df.shape[0]
+        factor = int(n_high / n_low)
+
+        range_lst = list(range(n_high))
+        col_names = list(series_df.columns)
+        midas_lst = []
+        for f in range(factor):
+            range_lst_tmp = range_lst[f:][0::factor]
+            series_tmp_df = series_df.iloc[range_lst_tmp, :]
+            series_tmp_df.index = low_index
+            col_names_tmp = [col_name + f'_{f}' for col_name in col_names]
+            rename_dict_tmp = dict(zip(col_names, col_names_tmp))
+            midas_lst += [series_tmp_df.rename(columns=rename_dict_tmp)]
+
+        midas_df = pd.concat(midas_lst, axis=1)
+
+        midas_ts = TimeSeries.from_dataframe(midas_df)
+
+        if strip:
+            midas_ts = midas_ts.strip()
+
+        return midas_ts
+
+
+# from darts.datasets import AirPassengersDataset
+
+# series = AirPassengersDataset().load()
+
