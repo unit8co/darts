d8ad5dc721e46fafc049cd3016612e9d514c7dfd
Alex Colpitts <alex.colpitts@gmail.com>
2023-06-26 14:51:06 -0400

    fixed isort

--- darts/models/__init__.py
+++ darts/models/__init__.py
@@ -36,8 +36,8 @@ try:
     from darts.models.forecasting.rnn_model import RNNModel
     from darts.models.forecasting.tcn_model import TCNModel
     from darts.models.forecasting.tft_model import TFTModel
-    from darts.models.forecasting.transformer_model import TransformerModel
     from darts.models.forecasting.tide_model import TiDEModel
+    from darts.models.forecasting.transformer_model import TransformerModel
 
 
 except ModuleNotFoundError:
