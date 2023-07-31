7f64c92e349ecb1b1dc306cdf5727848201d325e
Dennis Bader <dennis.bader@gmx.ch>
2023-07-31 16:57:25 +0200

    fix torch import on core requirements level (#1927)

--- darts/explainability/__init__.py
+++ darts/explainability/__init__.py
@@ -3,10 +3,22 @@ Explainability
 --------------
 """
 
+from darts.logging import get_logger
+
+logger = get_logger(__name__)
+
 from darts.explainability.explainability_result import (
     ShapExplainabilityResult,
     TFTExplainabilityResult,
     _ExplainabilityResult,
 )
 from darts.explainability.shap_explainer import ShapExplainer
-from darts.explainability.tft_explainer import TFTExplainer
+
+try:
+    from darts.explainability.tft_explainer import TFTExplainer
+except ModuleNotFoundError:
+    logger.warning(
+        "Support for Torch based explainers not available. "
+        'To enable them, install "darts", "u8darts[torch]" or "u8darts[all]" (with pip); '
+        'or "u8darts-torch" or "u8darts-all" (with conda).'
+    )
