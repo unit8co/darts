3ad5454342ffe379412124826e0a546233ed16ba
dennisbader <dennis.bader@gmx.ch>
2024-04-08 11:12:16 +0200

    fix failing unit tests

--- darts/tests/models/forecasting/test_probabilistic_models.py
+++ darts/tests/models/forecasting/test_probabilistic_models.py
@@ -201,7 +201,7 @@ if TORCH_AVAILABLE:
                 "output_chunk_length": 5,
                 "n_epochs": 100,
                 "random_state": 0,
-                "blocks": 1,
+                "num_blocks": 1,
                 "hidden_size": 32,
                 "dropout": 0.2,
                 "ff_size": 32,
