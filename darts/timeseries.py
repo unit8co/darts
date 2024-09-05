ade8ed9851f50b7aceeaca2eab88ea91dc2a7a6b
AlessioSymplectic <alessio.pellegrini@unit8.co>
2024-09-05 16:08:08 +0200

    * changed append and prepent with values to use expand_arr - as a side effect, we need to explicitly distinguish between empty and non-empty values

--- darts/timeseries.py
+++ darts/timeseries.py
@@ -55,7 +55,6 @@ from darts.logging import get_logger, raise_if, raise_if_not, raise_log
 from darts.utils import _build_tqdm_iterator, _parallel_apply
 from darts.utils.utils import (
     expand_arr,
-    expand_arr_comp_sample,
     generate_index,
     n_steps_between,
 )
@@ -2892,48 +2891,43 @@ class TimeSeries:
             A new TimeSeries with the new values appended
         """
 
-        values = np.array(values) if not isinstance(values, np.ndarray) else values
+        # If values is empty -> do nothing. Otherwise, expand values (if necessary) to match
+        # the dimensions
+        if len(values) == 0:
+            return self
 
-        # If the component and sample dimension don't match, we try to recast the values into the
-        # appropriate shape:
-        if values.shape[1:] != self._xa.values.shape[1:]:
-            logger.warning(
-                "The new values have a different component and sample dimension compared to the present series:"
-                f"Received : {values.shape[1:]}, expected: {self._xa.values.shape[1:]}."
-                "We try to recast the values into a fitting shape."
+        else:
+            values = np.array(values) if not isinstance(values, np.ndarray) else values
+            values = expand_arr(values, ndim=len(DIMS))
+            raise_if_not(
+                values.shape[1:] == self._xa.values.shape[1:],
+                "The values must have the same shape (components, sample) as the present series. "
+                f"Received: {values.shape[1:]}, expected: {self._xa.values.shape[1:]}",
             )
-            try:
-                values = expand_arr_comp_sample(values, self._xa.values.shape)
-                print(values)
-            except Exception:
-                raise_log(
-                    ValueError(
-                        "The reshaping of the values to match the series component and"
-                        "sample dimension cannot be performed."
-                    ),
-                    logger=logger,
+
+            if self._has_datetime_index:
+                idx = pd.DatetimeIndex(
+                    [
+                        self.end_time() + i * self._freq
+                        for i in range(1, len(values) + 1)
+                    ],
+                    freq=self._freq,
+                )
+            else:
+                idx = pd.RangeIndex(
+                    start=self.end_time() + self._freq,
+                    stop=self.end_time() + (len(values) + 1) * self._freq,
+                    step=self._freq,
                 )
 
-        if self._has_datetime_index:
-            idx = pd.DatetimeIndex(
-                [self.end_time() + i * self._freq for i in range(1, len(values) + 1)],
-                freq=self._freq,
+            return self.append(
+                self.__class__.from_times_and_values(
+                    values=values,
+                    times=idx,
+                    fill_missing_dates=False,
+                    static_covariates=self.static_covariates,
+                )
             )
-        else:
-            idx = pd.RangeIndex(
-                start=self.end_time() + self._freq,
-                stop=self.end_time() + (len(values) + 1) * self._freq,
-                step=self._freq,
-            )
-
-        return self.append(
-            self.__class__.from_times_and_values(
-                values=values,
-                times=idx,
-                fill_missing_dates=False,
-                static_covariates=self.static_covariates,
-            )
-        )
 
     def prepend(self, other: Self) -> Self:
         """
@@ -2975,53 +2969,45 @@ class TimeSeries:
             A new TimeSeries with the new values prepended.
         """
 
-        values = np.array(values) if not isinstance(values, np.ndarray) else values
+        # If values is empty -> do nothing. Otherwise, expand values (if necessary) to match
+        # the dimensions
+        if len(values) == 0:
+            return self
 
-        # If the component and sample dimension don't match, we try to recast the values into the
-        # appropriate shape:
-        if values.shape[1:] != self._xa.values.shape[1:]:
-            logger.warning(
-                "The new values have a different component and sample dimension compared to the present series:"
-                f"Received : {values.shape[1:]}, expected: {self._xa.values.shape[1:]}."
-                "We try to recast the values into a fitting shape."
+        else:
+            values = np.array(values) if not isinstance(values, np.ndarray) else values
+            values = expand_arr(values, ndim=len(DIMS))
+            raise_if_not(
+                values.shape[1:] == self._xa.values.shape[1:],
+                "The values must have the same shape (components, sample) as the present series. "
+                f"Received: {values.shape[1:]}, expected: {self._xa.values.shape[1:]}",
             )
-            try:
-                values = expand_arr_comp_sample(values, self._xa.values.shape)
-                print(values)
-            except Exception:
-                raise_log(
-                    ValueError(
-                        "The reshaping of the values to match the series component and"
-                        "sample dimension cannot be performed. "
-                    ),
-                    logger=logger,
+
+            if self._has_datetime_index:
+                idx = pd.DatetimeIndex(
+                    [
+                        self.start_time() - i * self._freq
+                        for i in reversed(range(1, len(values) + 1))
+                    ],
+                    freq=self._freq,
+                )
+            else:
+                idx = pd.RangeIndex(
+                    self.start_time() - self.freq * len(values),
+                    self.start_time(),
+                    step=self.freq,
                 )
 
-        if self._has_datetime_index:
-            idx = pd.DatetimeIndex(
-                [
-                    self.start_time() - i * self._freq
-                    for i in reversed(range(1, len(values) + 1))
-                ],
-                freq=self._freq,
+            return self.prepend(
+                self.__class__.from_times_and_values(
+                    values=values,
+                    times=idx,
+                    fill_missing_dates=False,
+                    static_covariates=self.static_covariates,
+                    columns=self.columns,
+                    hierarchy=self.hierarchy,
+                )
             )
-        else:
-            idx = pd.RangeIndex(
-                self.start_time() - self.freq * len(values),
-                self.start_time(),
-                step=self.freq,
-            )
-
-        return self.prepend(
-            self.__class__.from_times_and_values(
-                values=values,
-                times=idx,
-                fill_missing_dates=False,
-                static_covariates=self.static_covariates,
-                columns=self.columns,
-                hierarchy=self.hierarchy,
-            )
-        )
 
     def with_times_and_values(
         self,
