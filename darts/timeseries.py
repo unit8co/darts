36300f2849c8460e763a606f7cd4024b57fe6fd5
authierj <jules.authier@unit8.co>
2025-02-14 13:14:27 +0100

    from_series changed, names changed

--- darts/timeseries.py
+++ darts/timeseries.py
@@ -571,186 +571,6 @@ class TimeSeries:
     @classmethod
     def from_dataframe(
         cls,
-        df: pd.DataFrame,
-        time_col: Optional[str] = None,
-        value_cols: Optional[Union[list[str], str]] = None,
-        fill_missing_dates: Optional[bool] = False,
-        freq: Optional[Union[str, int]] = None,
-        fillna_value: Optional[float] = None,
-        static_covariates: Optional[Union[pd.Series, pd.DataFrame]] = None,
-        hierarchy: Optional[dict] = None,
-    ) -> Self:
-        """
-        Build a deterministic TimeSeries instance built from a selection of columns of a DataFrame.
-        One column (or the DataFrame index) has to represent the time,
-        and a list of columns `value_cols` has to represent the values for this time series.
-
-        Parameters
-        ----------
-        df
-            The DataFrame
-        time_col
-            The time column name. If set, the column will be cast to a pandas DatetimeIndex (if it contains
-            timestamps) or a RangeIndex (if it contains integers).
-            If not set, the DataFrame index will be used. In this case the DataFrame must contain an index that is
-            either a pandas DatetimeIndex, a pandas RangeIndex, or a pandas Index that can be converted to a
-            RangeIndex. It is better if the index has no holes; alternatively setting `fill_missing_dates` can in some
-            cases solve these issues (filling holes with NaN, or with the provided `fillna_value` numeric value, if
-            any).
-        value_cols
-            A string or list of strings representing the value column(s) to be extracted from the DataFrame. If set to
-            `None`, the whole DataFrame will be used.
-        fill_missing_dates
-            Optionally, a boolean value indicating whether to fill missing dates (or indices in case of integer index)
-            with NaN values. This requires either a provided `freq` or the possibility to infer the frequency from the
-            provided timestamps. See :meth:`_fill_missing_dates() <TimeSeries._fill_missing_dates>` for more info.
-        freq
-            Optionally, a string or integer representing the frequency of the underlying index. This is useful in order
-            to fill in missing values if some dates are missing and `fill_missing_dates` is set to `True`.
-            If a string, represents the frequency of the pandas DatetimeIndex (see `offset aliases
-            <https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases>`_ for more info on
-            supported frequencies).
-            If an integer, represents the step size of the pandas Index or pandas RangeIndex.
-        fillna_value
-            Optionally, a numeric value to fill missing values (NaNs) with.
-        static_covariates
-            Optionally, a set of static covariates to be added to the TimeSeries. Either a pandas Series or a pandas
-            DataFrame. If a Series, the index represents the static variables. The covariates are globally 'applied'
-            to all components of the TimeSeries. If a DataFrame, the columns represent the static variables and the
-            rows represent the components of the uni/multivariate TimeSeries. If a single-row DataFrame, the covariates
-            are globally 'applied' to all components of the TimeSeries. If a multi-row DataFrame, the number of
-            rows must match the number of components of the TimeSeries (in this case, the number of columns in
-            ``value_cols``). This adds control for component-specific static covariates.
-        hierarchy
-            Optionally, a dictionary describing the grouping(s) of the time series. The keys are component names, and
-            for a given component name `c`, the value is a list of component names that `c` "belongs" to. For instance,
-            if there is a `total` component, split both in two divisions `d1` and `d2` and in two regions `r1` and `r2`,
-            and four products `d1r1` (in division `d1` and region `r1`), `d2r1`, `d1r2` and `d2r2`, the hierarchy would
-            be encoded as follows.
-
-            .. highlight:: python
-            .. code-block:: python
-
-                hierarchy={
-                    "d1r1": ["d1", "r1"],
-                    "d1r2": ["d1", "r2"],
-                    "d2r1": ["d2", "r1"],
-                    "d2r2": ["d2", "r2"],
-                    "d1": ["total"],
-                    "d2": ["total"],
-                    "r1": ["total"],
-                    "r2": ["total"]
-                }
-            ..
-            The hierarchy can be used to reconcile forecasts (so that the sums of the forecasts at
-            different levels are consistent), see `hierarchical reconciliation
-            <https://unit8co.github.io/darts/generated_api/darts.dataprocessing.transformers.reconciliation.html>`_.
-
-        Returns
-        -------
-        TimeSeries
-            A univariate or multivariate deterministic TimeSeries constructed from the inputs.
-        """
-
-        # get values
-        if value_cols is None:
-            series_df = df.loc[:, df.columns != time_col]
-        else:
-            if isinstance(value_cols, str):
-                value_cols = [value_cols]
-            series_df = df[value_cols]  # slow
-
-        # get time index
-        if time_col:
-            if time_col not in df.columns:
-                raise_log(AttributeError(f"time_col='{time_col}' is not present."))
-
-            time_index = pd.Index([])
-            time_col_vals = df[time_col]
-
-            if np.issubdtype(time_col_vals.dtype, object):
-                # Try to convert to integers if needed
-                try:
-                    time_col_vals = time_col_vals.astype(int)
-                except ValueError:
-                    pass
-
-            if np.issubdtype(time_col_vals.dtype, np.integer):
-                # We have to check all integers appear only once to have a valid index
-                raise_if(
-                    time_col_vals.duplicated().any(),
-                    "The provided integer time index column contains duplicate values.",
-                )
-
-                # Temporarily use an integer Index to sort the values, and replace by a
-                # RangeIndex in `TimeSeries.from_xarray()`
-                time_index = pd.Index(time_col_vals)
-
-            elif np.issubdtype(time_col_vals.dtype, object):
-                # The integer conversion failed; try datetimes
-                try:
-                    time_index = pd.DatetimeIndex(time_col_vals)
-                except ValueError:
-                    raise_log(
-                        AttributeError(
-                            "'time_col' is of 'object' dtype but doesn't contain valid timestamps"
-                        )
-                    )
-            elif np.issubdtype(time_col_vals.dtype, np.datetime64):
-                time_index = pd.DatetimeIndex(time_col_vals)
-            else:
-                raise_log(
-                    AttributeError(
-                        "Invalid type of `time_col`: it needs to be of either 'str', 'datetime' or 'int' dtype."
-                    )
-                )
-            time_index.name = time_col
-        else:
-            raise_if_not(
-                isinstance(df.index, VALID_INDEX_TYPES)
-                or np.issubdtype(df.index.dtype, np.integer),
-                "If time_col is not specified, the DataFrame must be indexed either with "
-                "a DatetimeIndex, a RangeIndex, or an integer Index that can be converted into a RangeIndex",
-                logger,
-            )
-            # BUGFIX : force time-index to be timezone naive as xarray doesn't support it
-            # pandas.DataFrame loses the tz information if it's not its index
-            if isinstance(df.index, pd.DatetimeIndex) and df.index.tz is not None:
-                logger.warning(
-                    "The provided DatetimeIndex was associated with a timezone, which is currently not supported "
-                    "by xarray. To avoid unexpected behaviour, the tz information was removed. Consider calling "
-                    f"`ts.time_index.tz_localize({df.index.tz})` when exporting the results."
-                    "To plot the series with the right time steps, consider setting the matplotlib.pyplot "
-                    "`rcParams['timezone']` parameter to automatically convert the time axis back to the "
-                    "original timezone."
-                )
-                time_index = df.index.tz_localize(None)
-            else:
-                time_index = df.index
-
-        if not time_index.name:
-            time_index.name = time_col if time_col else DIMS[0]
-
-        if series_df.columns.name:
-            series_df.columns.name = None
-
-        xa = xr.DataArray(  # fast
-            series_df.values[:, :, np.newaxis],
-            dims=(time_index.name,) + DIMS[-2:],
-            coords={time_index.name: time_index, DIMS[1]: series_df.columns},
-            attrs={STATIC_COV_TAG: static_covariates, HIERARCHY_TAG: hierarchy},
-        )
-
-        return cls.from_xarray(  # slow
-            xa=xa,
-            fill_missing_dates=fill_missing_dates,
-            freq=freq,
-            fillna_value=fillna_value,
-        )
-
-    @classmethod
-    def from_narwhals_dataframe(
-        cls,
         df: IntoDataFrame,
         time_col: Optional[str] = None,
         value_cols: Optional[Union[list[str], str]] = None,
@@ -1152,61 +972,6 @@ class TimeSeries:
     @classmethod
     def from_series(
         cls,
-        pd_series: pd.Series,
-        fill_missing_dates: Optional[bool] = False,
-        freq: Optional[Union[str, int]] = None,
-        fillna_value: Optional[float] = None,
-        static_covariates: Optional[Union[pd.Series, pd.DataFrame]] = None,
-    ) -> Self:
-        """
-        Build a univariate deterministic series from a pandas Series.
-
-        The series must contain an index that is either a pandas DatetimeIndex, a pandas RangeIndex, or a pandas Index
-        that can be converted into a RangeIndex. It is better if the index has no holes; alternatively setting
-        `fill_missing_dates` can in some cases solve these issues (filling holes with NaN, or with the provided
-        `fillna_value` numeric value, if any).
-
-        Parameters
-        ----------
-        pd_series
-            The pandas Series instance.
-        fill_missing_dates
-            Optionally, a boolean value indicating whether to fill missing dates (or indices in case of integer index)
-            with NaN values. This requires either a provided `freq` or the possibility to infer the frequency from the
-            provided timestamps. See :meth:`_fill_missing_dates() <TimeSeries._fill_missing_dates>` for more info.
-        freq
-            Optionally, a string or integer representing the frequency of the underlying index. This is useful in order
-            to fill in missing values if some dates are missing and `fill_missing_dates` is set to `True`.
-            If a string, represents the frequency of the pandas DatetimeIndex (see `offset aliases
-            <https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases>`_ for more info on
-            supported frequencies).
-            If an integer, represents the step size of the pandas Index or pandas RangeIndex.
-        fillna_value
-            Optionally, a numeric value to fill missing values (NaNs) with.
-        static_covariates
-            Optionally, a set of static covariates to be added to the TimeSeries. Either a pandas Series or a
-            single-row pandas DataFrame. If a Series, the index represents the static variables. If a DataFrame, the
-            columns represent the static variables and the single row represents the univariate TimeSeries component.
-
-        Returns
-        -------
-        TimeSeries
-            A univariate and deterministic TimeSeries constructed from the inputs.
-        """
-        df = pd.DataFrame(pd_series)
-        return cls.from_dataframe(
-            df,
-            time_col=None,
-            value_cols=None,
-            fill_missing_dates=fill_missing_dates,
-            freq=freq,
-            fillna_value=fillna_value,
-            static_covariates=static_covariates,
-        )
-
-    @classmethod
-    def from_narwhals_series(
-        cls,
         pd_series: IntoSeries,
         fill_missing_dates: Optional[bool] = False,
         freq: Optional[Union[str, int]] = None,
@@ -1224,7 +989,7 @@ class TimeSeries:
         Parameters
         ----------
         pd_series
-            The pandas Series instance.
+            A Series instance.
         fill_missing_dates
             Optionally, a boolean value indicating whether to fill missing dates (or indices in case of integer index)
             with NaN values. This requires either a provided `freq` or the possibility to infer the frequency from the
@@ -1248,7 +1013,7 @@ class TimeSeries:
         TimeSeries
             A univariate and deterministic TimeSeries constructed from the inputs.
         """
-        nw_series = nw.from_native(pd_series, allow_series=True)
+        nw_series = nw.from_native(pd_series, series_only=True)
         df = nw_series.to_frame()
         return cls.from_dataframe(
             df,
