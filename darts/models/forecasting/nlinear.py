2e5835d3b1c9e9798c8e379a2330dbaf8f95e37d
Timon Erhart <57718207+turbotimon@users.noreply.github.com>
2025-04-07 15:29:17 +0200

    Update darts/models/forecasting/nlinear.py
    Co-authored-by: Dennis Bader <dennis.bader@gmx.ch>

--- darts/models/forecasting/nlinear.py
+++ darts/models/forecasting/nlinear.py
@@ -242,7 +242,6 @@ class NLinearModel(MixedCovariatesTorchModel):
             .. note::
                 This cannot be applied to probabilistic models.
             ..
-
         use_static_covariates
             Whether the model should use static covariate information in case the input `series` passed to ``fit()``
             contain static covariates. If ``True``, and static covariates are available at fitting time, will enforce
