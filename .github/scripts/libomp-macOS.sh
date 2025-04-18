bd0be79643d5d182891b0c88d6d011d0223a9780
dennisbader <dennis.bader@gmx.ch>
2025-04-18 14:56:19 +0200

    add back libomp for python 3.11 checks

---
+++ .github/scripts/libomp-macOS.sh
@@ -0,0 +1,4 @@
+brew install libomp wget
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/fb8323f2b170bd4ae97e1bac9bf3e2983af3fdb0/Formula/libomp.rb
+brew unlink libomp
+brew install libomp.rb
