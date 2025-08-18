d4df874c3ee7fa6d528cf9da9119756d7fffc9a8
dennisbader <dennis.bader@gmx.ch>
2025-08-18 13:37:37 +0200

    add back tests to darts package source for conda build

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     maintainer="Unit8 SA",
     maintainer_email="darts@unit8.co",
     license="Apache License 2.0",
-    packages=find_packages(exclude=["examples*", "darts.tests*"]),
+    packages=find_packages(exclude=["examples*"]),
     install_requires=reqs,
     package_data={
         "darts": ["py.typed"],
