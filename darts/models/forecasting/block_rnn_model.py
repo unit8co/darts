6b4d3dcfa441a41cf71bac24c375a88ee241cc79
dennisbader <dennis.bader@gmx.ch>
2025-08-22 14:35:44 +0200

    clean up

--- darts/models/forecasting/block_rnn_model.py
+++ darts/models/forecasting/block_rnn_model.py
@@ -318,6 +318,10 @@ class BlockRNNModel(MixedCovariatesTorchModel):
         activation
             The name of a torch.nn activation function to be applied between the layers of the fully connected network.
             Default: "ReLU".
+        use_static_covariates
+            Whether the model should use static covariate information in case the input `series` passed to ``fit()``
+            contain static covariates. If ``True``, and static covariates are available at fitting time, will enforce
+            that all target `series` have the same static covariate dimensionality in ``fit()`` and ``predict()``.
         **kwargs
             Optional arguments to initialize the pytorch_lightning.Module, pytorch_lightning.Trainer, and
             Darts' :class:`TorchForecastingModel`.
