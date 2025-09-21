c087d6a6b8314ba66f86a4ffabe94f6668882acf
Tim Rosenflanz <trosenflanz@gmail.com>
2025-09-21 10:45:50 +0000

    improve parameter checking by considering the full class hierarchy

--- darts/models/forecasting/torch_forecasting_model.py
+++ darts/models/forecasting/torch_forecasting_model.py
@@ -362,14 +362,24 @@ class TorchForecastingModel(GlobalForecastingModel, ABC):
 
     @classmethod
     def _validate_model_params(cls, **kwargs):
-        """validate that parameters used at model creation are part of :class:`TorchForecastingModel`,
-        :class:`PLForecastingModule` or cls __init__ methods.
-        """
-        valid_kwargs = (
-            set(inspect.signature(TorchForecastingModel.__init__).parameters.keys())
-            | set(inspect.signature(PLForecastingModule.__init__).parameters.keys())
-            | set(inspect.signature(cls.__init__).parameters.keys())
+        # initiate with PLForecastingModule params that isn't part of the base class
+        valid_kwargs = set(
+            inspect.signature(PLForecastingModule.__init__).parameters.keys()
         )
+        # add params from the full list of base classes
+        for base in inspect.getmro(cls):
+            if base is object:
+                break
+            try:
+                sig = inspect.signature(base.__init__)
+                valid_kwargs.update(sig.parameters.keys())
+            except (ValueError, TypeError):
+                # In case a built-in class throws or __init__ is not introspectable
+                continue
+
+        # Remove 'self','args,'kwargs' from consideration
+        for generic_arg in ["self", "args", "kwargs"]:
+            valid_kwargs.discard(generic_arg)
 
         invalid_kwargs = [kwarg for kwarg in kwargs if kwarg not in valid_kwargs]
 
