bbfd70d9de395e9c320f3e80a46df565419bc841
Zhihao Dai <zhihao.dai@eng.ox.ac.uk>
2025-11-08 22:54:52 +0000

    Chronos-2: fix docstring typo
    Co-authored-by: Zhihao Dai <zhihao.dai@eng.ox.ac.uk>

--- darts/models/forecasting/chronos2_model.py
+++ darts/models/forecasting/chronos2_model.py
@@ -620,7 +620,7 @@ class Chronos2Model(FoundationModel, HuggingFaceModelMixin):
             cannot generate autoregressive predictions (`n > output_chunk_length`).
         likelihood
             The likelihood model to be used for probabilistic forecasts. Must be ``None`` or an instance of
-            :class:`darts.utils.likelihood_models.torch.QuantileRegression`. If using ``QuantileRegression``,
+            :class:`~darts.utils.likelihood_models.torch.QuantileRegression`. If using ``QuantileRegression``,
             the quantiles must be a subset of those used during Chronos-2 pre-training:
             [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9,
             0.95, 0.99].
