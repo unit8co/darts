a09e650ed7e694a4fef7bd0036ccfb4d8ce1f869
Zhihao Dai <zhihao.dai@eng.ox.ac.uk>
2025-12-26 21:51:55 +0000

    Add note for fidelity test
    Co-authored-by: Zhihao Dai <zhihao.dai@eng.ox.ac.uk>

--- darts/tests/models/forecasting/test_timesfm2p5.py
+++ darts/tests/models/forecasting/test_timesfm2p5.py
@@ -18,7 +18,7 @@ from darts.models import TimesFM2p5Model
 from darts.tests.conftest import TORCH_AVAILABLE, tfm_kwargs
 from darts.utils.likelihood_models import QuantileRegression
 
-# quantiles used during Chronos-2 pre-training
+# quantiles used during TimesFM 2.5 pre-training
 all_quantiles = [
     0.1,
     0.2,
@@ -33,7 +33,7 @@ all_quantiles = [
 
 
 def load_validation_inputs():
-    """Load validation inputs for Chronos2Model fidelity tests. The data imports
+    """Load validation inputs for TimesFM2p5Model fidelity tests. The data imports
     here are adapted from the `20-SKLearnModel-examples` notebook.
     """
     # convert to float32 due to MPS not supporting float64
@@ -67,10 +67,59 @@ class TestTimesFM2p5Model:
         Original predictions were generated with the following code:
 
         ```python
-        TODO
+        import numpy as np
+        import pandas as pd
+        import timesfm
+        import torch
+
+        from darts.datasets import ElectricityConsumptionZurichDataset
+
+        # adapted from `20-SKLearnModel-examples` notebook
+        ts_energy = ElectricityConsumptionZurichDataset().load().astype(np.float32)
+
+        # extract households energy consumption
+        ts_energy = ts_energy[["Value_NE5", "Value_NE7"]]
+
+        # create train and validation splits
+        validation_cutoff = pd.Timestamp("2022-01-01")
+        ts_energy_train, ts_energy_val = ts_energy.split_after(validation_cutoff)
+
+        # set torch precision
+        torch.set_float32_matmul_precision("high")
+
+        # load TimesFM 2.5 model
+        model = timesfm.TimesFM_2p5_200M_torch.from_pretrained("google/timesfm-2.5-200m-pytorch")
+
+        # compile the model with forecast configuration
+        model.compile(
+            timesfm.ForecastConfig(
+                max_context=1024,
+                max_horizon=256,
+                normalize_inputs=False,
+                use_continuous_quantile_head=False,
+                force_flip_invariance=False,
+                infer_is_positive=False,
+                fix_quantile_crossing=False,
+            )
+        )
+        point_forecast, quantile_forecast = model.forecast(
+            horizon=128, # use output_patch_len here to prevent auto-regressive forecasting
+            inputs=[
+                series
+                for series in ts_energy_train.values().T
+            ], # Two energy consumption series
+        )
+
+        # convert to numpy array with shape (time, variables, quantiles)
+        quantile_forecast = quantile_forecast.transpose(1, 0, 2)
+        # exclude the mean forecast (first quantile)
+        quantile_forecast = quantile_forecast[:, :, 1:]
+
+        # save quantiles to a npz file
+        np.savez_compressed("timesfm2p5.npz", pred=quantile_forecast)
         ```
 
-        Code accessed from TODO
+        Code accessed from https://github.com/google-research/timesfm/commit/6bd8044275f8b76cdc9554f2fecccac5f31a156c
         on 26th December 2025.
 
         """
