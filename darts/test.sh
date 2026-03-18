542e59d30646dbc7a3fd422c79164807afb984c8
Zhihao Dai <zhihao.dai@eng.ox.ac.uk>
2026-03-18 21:18:19 +0000

    Remove unused files
    Co-authored-by: Zhihao Dai <zhihao.dai@eng.ox.ac.uk>

--- darts/test.sh
+++
@@ -1,2 +0,0 @@
-uv run pytest --durations=50 --cov=darts --cov-config=.coveragerc --cov-report=xml darts/tests/explainability/test_torch_explainer.py
-uv run pytest --durations=50 --cov=darts --cov-config=.coveragerc --cov-report=xml darts/tests/explainability/test_sklearn_explainer.py
